f274d7efc6da4845dd85086971afc4d0f310d616
OctopusTentacles <125494033+OctopusTentacles@users.noreply.github.com>
2023-02-17 22:22:04 +0500

    One line output

---
+++ Part1_Modul02/2_3_concatenation.py
@@ -0,0 +1,7 @@
+name = input('Введите имя: ')
+print('Привет, ', name + "!")
+
+first_word = input("Введите первое слово: ")
+second_word = input("Введите второе слово: ")
+print(first_word, second_word)
+print(first_word + " " + second_word)
