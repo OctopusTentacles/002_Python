3700a2f6750c387594b7dec5e32aa9db3d716a76
Octopus <klinov.artem@gmail.com>
2023-03-12 22:15:27 +0500

    task 4

--- Part1_Modul08/8_6_task_4.py
+++ Part1_Modul08/8_6_task_4.py
@@ -4,14 +4,14 @@ print('Задача 4. Отрезок')
 # считает и выводит на консоль среднее арифметическое всех чисел
 # из отрезка [a; b], кратных числу c.
 
-numberA = int(input("Введите число а: "))
-numberB = int(input("Введите число b: "))
-numberC = int(input("Введите число c: "))
+numberA = int(input("Введите начало отрезка: "))
+numberB = int(input("Введите конец отрезка: "))
+numberC = int(input("Введите делитель: "))
 sumNumber = 0
 count = 0
 for i in range(numberA, numberB + 1, 1):
-    if i // numberC == 0:
+    if i % numberC == 0:
         print(i)
         sumNumber += i
         count += 1
-print("среднее арифметическое всех чисел", sumNumber / count)
+print("Среднее арифметическое всех чисел кратных делителю:", sumNumber / count)
