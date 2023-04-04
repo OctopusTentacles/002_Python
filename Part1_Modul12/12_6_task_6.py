035987b5cae183d260177e8b2a114fb9a1c6d1cd
Octopus <klinov.artem@gmail.com>
2023-04-04 12:08:15 +0600

    draft

--- Part1_Modul12/12_6_task_6.py
+++ Part1_Modul12/12_6_task_6.py
@@ -5,6 +5,12 @@ print('Задача 6. НОД')
 num_1 = int(input('Введите первое число: '))
 num_2 = int(input('Введите второе число: '))
 
-for i in range(2, 9):
+
+if num_1 > num_2:
+    point = num_2
+else:
+    point = num_1
+
+for i in range(1, point + 1):
     if (num_1 % i == 0) and (num_2 % i == 0):
         print('наибольший общий делитель', i)
