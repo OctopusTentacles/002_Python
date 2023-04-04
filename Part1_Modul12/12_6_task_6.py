d674361aeb350aefcf03d5de617a48b4e1bb71b6
Octopus <klinov.artem@gmail.com>
2023-04-04 14:55:55 +0600

    Task 6. Greatest Common Divisor (наиб общ делит)

--- Part1_Modul12/12_6_task_6.py
+++ Part1_Modul12/12_6_task_6.py
@@ -2,15 +2,19 @@ print('Задача 6. НОД')
 
 # Напишите функцию, вычисляющую наибольший общий делитель двух чисел
 
+
+def NOD(n1, n2):
+    if n1 > n2:
+        point = n2
+    else:
+        point = n1
+
+    for i in range(1, point + 1):
+        if (n1 % i == 0) and (n2 % i == 0):
+            nod = i
+    print('Наибольший общий делитель:', nod)
+
+
 num_1 = int(input('Введите первое число: '))
 num_2 = int(input('Введите второе число: '))
-
-
-if num_1 > num_2:
-    point = num_2
-else:
-    point = num_1
-
-for i in range(1, point + 1):
-    if (num_1 % i == 0) and (num_2 % i == 0):
-        print('наибольший общий делитель', i)
+NOD(num_1, num_2)
