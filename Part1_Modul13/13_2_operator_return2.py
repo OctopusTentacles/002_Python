6bcd6c69d6fc5f8a3c765b9e615f7cbe17293aca
Octopus <klinov.artem@gmail.com>
2023-04-06 20:18:07 +0600

    Greatest common divisor (NOD2)

--- Part1_Modul13/13_2_operator_return2.py
+++ Part1_Modul13/13_2_operator_return2.py
@@ -19,10 +19,12 @@ def gcd(a, b):
     else:
         small = a
     for i in range(1, small + 1):
-        if (a % i=0) and
+        if (a % i == 0) and (b % i == 0):
+            nod = i
+    return nod
 
 
 num1 = int(input('Введите первое число: '))
 num2 = int(input('Введите второе число: '))
 
-print('НОД = ', gcd(num1, num2))
+print('\nНОД = ', gcd(num1, num2))
