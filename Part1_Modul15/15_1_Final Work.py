df8b835ac21fae8adcf97ecc0e35510024d37cf1
Octopus <klinov.artem@gmail.com>
2023-04-12 22:23:57 +0500

    draft

--- Part1_Modul15/15_1_Final Work.py
+++ Part1_Modul15/15_1_Final Work.py
@@ -7,30 +7,38 @@ n = ''
 a = 0
 ph = ''
 e = ''
+index = ''
+adress = ''
 i = ''
-# social links
-v = ''
-t = ''
-tk = ''
+# business info
+ogrnip = ''
+inn = ''
+account = ''
+bank = ''
+bik = ''
+account2 = ''
 
 
 def general_info_user(n_parameter, a_parameter, ph_parameter, e_parameter, i_parameter):
     print(SEPARATOR)
     print('Имя:    ', n_parameter)
-    if 11 <= a_parameter % 100 <= 19: years_parameter = 'лет'
-    elif a_parameter % 10 == 1: years_parameter = 'год'
-    elif 2 <= a_parameter % 10 <= 4: years_parameter = 'года'
-    else: years_parameter = 'лет'
-
+    if 11 <= a_parameter % 100 <= 19:
+        years_parameter = 'лет'
+    elif a_parameter % 10 == 1:
+        years_parameter = 'год'
+    elif 2 <= a_parameter % 10 <= 4:
+        years_parameter = 'года'
+    else:
+        years_parameter = 'лет'
 
     print('Возраст:', a_parameter, years_parameter)
     print('Телефон:', ph_parameter)
     print('E-mail: ', e_parameter)
     if i:
-            print('')
-            print('Дополнительная информация:')
-            print(i_parameter)
-    
+        print('')
+        print('Дополнительная информация:')
+        print(i_parameter)
+
 
 print('Приложение MyProfile')
 print('Сохраняй информацию о себе и выводи ее в разных форматах')
@@ -45,7 +53,7 @@ while True:
 
     option = int(input('Введите номер пункта меню: '))
     if option == 0:
-            break
+        break
 
     if option == 1:
         # submenu 1: edit info
@@ -53,7 +61,7 @@ while True:
             print(SEPARATOR)
             print('ВВЕСТИ ИЛИ ОБНОВИТЬ ИНФОРМАЦИЮ')
             print('1 - Общая информация')
-            print('2 - Социальные сети и мессенджеры')
+            print('2 - Информация о предпринимателе')
             print('0 - Назад')
 
             option2 = int(input('Введите номер пункта меню: '))
@@ -63,11 +71,11 @@ while True:
                 # input general info
                 n = input('Введите имя: ')
                 while 1:
-                        # validate user age
-                        a = int(input('Введите возраст: '))
-                        if a > 0:
-                            break
-                        print('Возраст должен быть положительным')
+                    # validate user age
+                    a = int(input('Введите возраст: '))
+                    if a > 0:
+                        break
+                    print('Возраст должен быть положительным')
 
                 uph = input('Введите номер телефона (+7ХХХХХХХХХХ): ')
                 ph = ''
@@ -75,16 +83,22 @@ while True:
                     if ch == '+' or ('0' <= ch <= '9'):
                         ph += ch
 
-
                 e = input('Введите адрес электронной почты: ')
+                index = input('Введите почтовый индекс: ')
+                adress = input('Введите почтовый адрес (без индекса): ')
                 i = input('Введите дополнительную информацию:\n')
 
             elif option2 == 2:
-                # input social links
-                v = input('Введите адрес профиля Вконтакте: ')
-                t = input('Введите логин Telegram: ')
-                tk = input('Введите логин Tiktok: ')
-            else: print('Введите корректный пункт меню')
+                # input business info
+                ogrnip = input('Введите ОГРНИП: ')
+                inn = input('Введите ИИН: ')
+                account = input('Введите расчетный счет: ')
+                bank = input('Введите название банка: ')
+                bik = input('Введите БИК: ')
+                account2 = input('Введите корреспондентский счет: ')
+
+            else:
+                print('Введите корректный пункт меню')
     elif option == 2:
         # submenu 2: print info
         while True:
@@ -109,5 +123,7 @@ while True:
                 print('Вконтакте:', v)
                 print('Telegram: ', t)
                 print('Tiktok:   ', tk)
-            else:   print('Введите корректный пункт меню')
-    else:       print('Введите корректный пункт меню')
+            else:
+                print('Введите корректный пункт меню')
+    else:
+        print('Введите корректный пункт меню')
