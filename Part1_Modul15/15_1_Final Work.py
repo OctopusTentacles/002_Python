bddbf6f6b02a2a0bf35e0eaa243285aa15c9420e
Octopus Tentacles <klinov.artem@gmail.com>
2023-04-13 12:41:39 +0500

    draft

--- Part1_Modul15/15_1_Final Work.py
+++ Part1_Modul15/15_1_Final Work.py
@@ -19,26 +19,26 @@ bik = ''
 account2 = ''
 
 
-def general_info_user(n_parameter, a_parameter, 
-                      ph_parameter, e_parameter, i_parameter):
+def general_info_user(name_parameter, age_parameter, 
+                      phone_parameter, email_parameter, info_parameter):
     print(SEPARATOR)
-    print('Имя:    ', n_parameter)
-    if 11 <= a_parameter % 100 <= 19:
+    print('Имя:    ', name_parameter)
+    if 11 <= age_parameter % 100 <= 19:
         years_parameter = 'лет'
-    elif a_parameter % 10 == 1:
+    elif age_parameter % 10 == 1:
         years_parameter = 'год'
-    elif 2 <= a_parameter % 10 <= 4:
+    elif 2 <= age_parameter % 10 <= 4:
         years_parameter = 'года'
     else:
         years_parameter = 'лет'
 
-    print('Возраст:', a_parameter, years_parameter)
-    print('Телефон:', ph_parameter)
-    print('E-mail: ', e_parameter)
-    if i:
+    print('Возраст:', age_parameter, years_parameter)
+    print('Телефон:', phone_parameter)
+    print('E-mail: ', email_parameter)
+    if info:
         print('')
         print('Дополнительная информация:')
-        print(i_parameter)
+        print(info_parameter)
 
 
 def business_info_user(ogrnip_parameter, inn_parameter, account_parameter, bank_parameter, bik_parameter, account2_parameter):
@@ -56,8 +56,9 @@ def business_info_user(ogrnip_parameter, inn_parameter, account_parameter, bank_
 print('Приложение MyProfile')
 print('Сохраняй информацию о себе и выводи ее в разных форматах')
 
-while True:
-    # main menu
+
+# MAIN MENU
+while True:    
     print(SEPARATOR)
     print('ГЛАВНОЕ МЕНЮ')
     print('1 - Ввести или обновить информацию')
@@ -69,9 +70,9 @@ while True:
         break
 
 
-
+    # SUBMENU 1: EDIT INFO
     if option == 1:
-        # submenu 1: edit info
+        
         while True:
             print(SEPARATOR)
             print('ВВЕСТИ ИЛИ ОБНОВИТЬ ИНФОРМАЦИЮ')
@@ -84,30 +85,28 @@ while True:
                 break
 
             # INPUT GENERAL INFO
-
             if option2 == 1:
                 
                 name = input('Введите имя: ')
                 while 1:
                     # validate user age
                     age = int(input('Введите возраст: '))
-                    if a > 0:
+                    if age > 0:
                         break
                     print('Возраст должен быть положительным')
 
-                uph = input('Введите номер телефона (+7ХХХХХХХХХХ): ')
-                ph = ''
-                for ch in uph:
+                user_phone = input('Введите номер телефона (+7ХХХХХХХХХХ): ')
+                phone = ''
+                for ch in user_phone:
                     if ch == '+' or ('0' <= ch <= '9'):
-                        ph += ch
-                e = input('Введите адрес электронной почты: ')
+                        phone += ch
+                email = input('Введите адрес электронной почты: ')
                 index = input('Введите почтовый индекс: ')
                 adress = input('Введите почтовый адрес (без индекса): ')
-                i = input('Введите дополнительную информацию:\n')
+                info = input('Введите дополнительную информацию:\n')
 
 
             # INPUT BUSINESS INFO
-
             elif option2 == 2:                
                 while True:
                     count = 0
@@ -135,7 +134,6 @@ while True:
 
 
     # SUBMENU 2: PRINT INFO
-
     elif option == 2:
         
         while True:
@@ -148,12 +146,12 @@ while True:
             option2 = int(input('Введите номер пункта меню: '))
             if option2 == 0:
                 break
+            
             if option2 == 1:
-                general_info_user(n, a, ph, e, i)
+                general_info_user(name, age, phone, email, info)
 
             elif option2 == 2:
-                general_info_user(n, a, ph, e, i)
-
+                general_info_user(name, age, phone, email, info)
                 business_info_user(ogrnip, inn, account, bank, bik, account2)
 
             else:
