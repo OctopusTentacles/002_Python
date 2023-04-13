8f6a37aae0b0d51a0a4ce6191c2f163d02621d34
Octopus Tentacles <klinov.artem@gmail.com>
2023-04-13 09:48:02 +0500

    draft

--- Part1_Modul15/15_1_Final Work.py
+++ Part1_Modul15/15_1_Final Work.py
@@ -39,9 +39,11 @@ def general_info_user(n_parameter, a_parameter, ph_parameter, e_parameter, i_par
         print('Дополнительная информация:')
         print(i_parameter)
 
-
 print('Приложение MyProfile')
 print('Сохраняй информацию о себе и выводи ее в разных форматах')
+
+
+
 
 while True:
     # main menu
@@ -55,6 +57,8 @@ while True:
     if option == 0:
         break
 
+
+
     if option == 1:
         # submenu 1: edit info
         while True:
@@ -67,6 +71,7 @@ while True:
             option2 = int(input('Введите номер пункта меню: '))
             if option2 == 0:
                 break
+
             if option2 == 1:
                 # input general info
                 n = input('Введите имя: ')
@@ -82,23 +87,40 @@ while True:
                 for ch in uph:
                     if ch == '+' or ('0' <= ch <= '9'):
                         ph += ch
-
                 e = input('Введите адрес электронной почты: ')
                 index = input('Введите почтовый индекс: ')
                 adress = input('Введите почтовый адрес (без индекса): ')
                 i = input('Введите дополнительную информацию:\n')
 
             elif option2 == 2:
+
                 # input business info
-                ogrnip = input('Введите ОГРНИП: ')
+                while True:
+                    count = 0
+                    ogrnip = input('Введите ОГРНИП: ')
+                    for i in ogrnip:
+                        count += 1                        
+                    if count == 15:
+                        break
+                    print('ОГРНИП должен содержать 15 цифр')
                 inn = input('Введите ИИН: ')
-                account = input('Введите расчетный счет: ')
+                while True:
+                    count = 0
+                    account = input('Введите расчетный счет: ')
+                    for i in account:
+                        count += 1                        
+                    if count == 20:
+                        break
+                    print('Расчетный счет должен содержать 20 цифр')
                 bank = input('Введите название банка: ')
                 bik = input('Введите БИК: ')
                 account2 = input('Введите корреспондентский счет: ')
 
             else:
                 print('Введите корректный пункт меню')
+
+
+
 
     elif option == 2:
         # submenu 2: print info
