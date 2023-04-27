fdf6b353aff73c7f9873e818365f886ee73cfa93
Octopus Tentacles <klinov.artem@gmail.com>
2023-04-27 15:13:37 +0500

    Text colors

---
+++ colors.py
@@ -0,0 +1,28 @@
+print("Default")
+
+print("\033[30m", end="") #dark
+print("dark")
+
+print("\033[31m", end="") #red
+print("red")
+
+print("\033[32m", end="") #green
+print("green")
+
+print("\033[33m", end="") #yellow
+print("yellow")
+
+print("\033[34m", end="") #blue
+print("blue")
+
+print("\033[35m", end="") #purple
+print("purple")
+
+print("\033[36m", end="") #green-blue
+print("green-blue")
+
+print("\033[37m", end="") #grey
+print("grey")
+
+print("\033[0m", end="") #default
+print("default")
