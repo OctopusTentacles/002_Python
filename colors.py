68c86b5c5b65a0f89c4a922990d3bd8df13324da
Octopus <klinov.artem@gmail.com>
2023-04-28 22:09:04 +0500

    Squashed commit of the following:
    commit abdd5b5017f8d712a9c0bdcb921ba9e3d3ce502c
    Author: Octopus <klinov.artem@gmail.com>
    Date:   Fri Apr 28 22:01:07 2023 +0500
    
        added rows
    
    commit 43e7a7d49fc8b379dd441e207a0f54529decd9e8
    Author: Octopus Tentacles <125494033+OctopusTentacles@users.noreply.github.com>
    Date:   Thu Apr 27 17:12:36 2023 +0500
    
        deleted examples
    
    commit 7af54b9fd138a99c031a793c8ad36609771d75db
    Author: Octopus Tentacles <klinov.artem@gmail.com>
    Date:   Thu Apr 27 16:38:53 2023 +0500
    
        text colors added new

--- colors.py
+++ colors.py
@@ -1,28 +1,56 @@
-print("Default")
+ 
 
-print("\033[30m", end="") #dark
-print("dark")
 
-print("\033[31m", end="") #red
-print("red")
 
-print("\033[32m", end="") #green
-print("green")
 
-print("\033[33m", end="") #yellow
-print("yellow")
 
-print("\033[34m", end="") #blue
-print("blue")
 
-print("\033[35m", end="") #purple
-print("purple")
 
-print("\033[36m", end="") #cyan
-print("cyan")
 
-print("\033[37m", end="") #grey
-print("grey")
 
-print("\033[0m", end="") #default
-print("default")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+""" ANSI color codes """
+
+BLACK = "\033[0;30m"
+RED = "\033[0;31m"
+GREEN = "\033[0;32m"
+BROWN = "\033[0;33m"
+BLUE = "\033[0;34m"
+PURPLE = "\033[0;35m"
+CYAN = "\033[0;36m"
+LIGHT_GRAY = "\033[0;37m"
+DARK_GRAY = "\033[1;30m"
+LIGHT_RED = "\033[1;31m"
+LIGHT_GREEN = "\033[1;32m"
+YELLOW = "\033[1;33m"
+LIGHT_BLUE = "\033[1;34m"
+LIGHT_PURPLE = "\033[1;35m"
+LIGHT_CYAN = "\033[1;36m"
+LIGHT_WHITE = "\033[1;37m"
+BOLD = "\033[1m"
+FAINT = "\033[2m"
+ITALIC = "\033[3m"
+UNDERLINE = "\033[4m"
+BLINK = "\033[5m"
+NEGATIVE = "\033[7m"
+CROSSED = "\033[9m"
+END = "\033[0m"
