80c180c4f331a92b5bf44a566931eb08c3bf4135
Octopus <klinov.artem@gmail.com>
2023-05-01 22:07:30 +0500

    import token

--- TRAINING/BootCamp/telegramBot.py
+++ TRAINING/BootCamp/telegramBot.py
@@ -2,4 +2,7 @@
 # pip3 install pyTelegramBotAPI
 
 import telebot
-token = "наш токен"
+token = "5813295603:AAF3y5Y2XUZDz-dzXfyl8evXBrm3ZHMADQg"
+
+# В этой строчке мы заводим бота и даем ему запомнить токен
+bot = telebot.TeleBot(token)
