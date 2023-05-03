195a9e15a9444c82eb022130cf8061d335932b70
Octopus <klinov.artem@gmail.com>
2023-05-03 21:36:24 +0500

    02-bot-greating

--- TRAINING/BootCamp/telegramBot.py
+++ TRAINING/BootCamp/telegramBot.py
@@ -2,7 +2,19 @@
 # pip3 install pyTelegramBotAPI
 
 import telebot
+
 token = "5813295603:AAF3y5Y2XUZDz-dzXfyl8evXBrm3ZHMADQg"
 
 # В этой строчке мы заводим бота и даем ему запомнить токен
 bot = telebot.TeleBot(token)
+
+# Пишем первую функцию, которая отвечает "Привет" на команду /start
+# Все функции общения приложения с ТГ спрятаны в функции под @
+@bot.message_handler(commands=['start'])
+
+def say_hi(message):
+    bot.send_message(message.chat.id, 'Привет ✌️ ')
+
+# Запускаем бота. Он будет работать до тех пор, пока работает ячейка (крутится значок слева).
+# Остановим ячейку - остановится бот
+bot.infinity_poling()
