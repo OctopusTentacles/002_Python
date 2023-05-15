7cbc0931496a1f5a2723e35af94b6862590c4085
Octopus <klinov.artem@gmail.com>
2023-05-15 22:26:24 +0500

    draft

--- TRAINING/Boot_Camp/telegramBot.py
+++ TRAINING/Boot_Camp/telegramBot.py
@@ -9,14 +9,12 @@ import os
 
 token = "5813295603:AAF3y5Y2XUZDz-dzXfyl8evXBrm3ZHMADQg"
 
-
 # В этой строчке мы заводим бота и даем ему запомнить токен
 bot = telebot.TeleBot(token)
 
 # Пишем первую функцию, которая отвечает "Привет" на команду /start
 # Все функции общения приложения с ТГ спрятаны в функции под @
 @bot.message_handler(commands=['start'])
-
 def say_hi(message):
     bot.send_message(message.chat.id, 'Привет ✌️ ' + message.chat.first_name)   #add user name
 
