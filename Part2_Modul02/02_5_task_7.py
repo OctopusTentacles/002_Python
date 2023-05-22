aad668c7b51d07871fd210f2b5d172093dbcc0b3
Octopus <klinov.artem@gmail.com>
2023-05-22 14:19:06 +0600

    draft

--- Part2_Modul02/02_5_task_7.py
+++ Part2_Modul02/02_5_task_7.py
@@ -21,4 +21,5 @@ word = list(input('Введите слово: '))
 for index in range(len(word)//2):
     if word[index] != word[-1 - index]:
         print('Слово не является палиндромом')
+        break
     
