caf1ca11ec384e92c820b2681493eff25fc85f45
Octopus <klinov.artem@gmail.com>
2023-05-23 21:21:54 +0600

    method list - insert, remove, index

--- Part2_Modul03/03_2_methods1.py
+++ Part2_Modul03/03_2_methods1.py
@@ -25,3 +25,5 @@ zoo.insert(1, 'bear')
 zoo.remove('elephant')
 
 print('Зоопарк', zoo)
+print('Лев сидит в клетке номер', zoo.index('lion') +1)
+print('Обезьяна сидит в клетке номер', zoo.index('monkey') + 1)
