1babff93469ee0be4d22ce2bcafb86463542c11a
Octopus <klinov.artem@gmail.com>
2023-05-25 21:56:11 +0600

    Scary code / Extend, Remove

--- Part2_Modul03/03_5_task_1.py
+++ Part2_Modul03/03_5_task_1.py
@@ -27,8 +27,11 @@ side1_list = [1, 5, 1, 5]
 side2_list = [1, 3, 1, 5, 3, 3]
 
 main_list.extend(side1_list)
-print('\nКол-во цифр 5 при первом объединении:', main_list.count(5))
-main_list.remove(5)
+five_count = main_list.count(5)
+print('\nКол-во цифр 5 при первом объединении:', five_count)
+
+for _ in range(five_count):
+    main_list.remove(5)
 
 main_list.extend(side2_list)
 print('\nКол-во цифр 3 при втором объединении:', main_list.count(3))
