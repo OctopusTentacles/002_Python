c9f754f8e8cbd53998d96e22ad556947582b9605
Octopus <klinov.artem@gmail.com>
2023-06-03 11:42:33 +0600

    counting rhyme / list / count

--- Part2_Modul03/03_5_task_7.py
+++ Part2_Modul03/03_5_task_7.py
@@ -38,14 +38,16 @@ k_number = int(input('Какое число в считалке? '))
 print(f'Значит, выбывает каждый {k_number}-й человек!')
 
 people_list = list(range(1, n_people + 1))
-count = 0
+srart_number = 0
+drop_man = 0
 
 while len(people_list) > 1:
     print('\nТекущий круг людей:', people_list)
     
-    start_number = count % len(people_list) + 1
-    print('Начало счета с номера', start_number)
+    srart_number = drop_man + 1
+    print('Начало счета с номера', srart_number)
 
-    count = (start_number + k_number -1) % len(people_list)
-    print('Выбывает человек под номером', count)
-    people_list.remove(count)
+    drop_man = (srart_number + k_number - 1) % len(people_list)
+    
+    print('Выбывает человек под номером', drop_man)
+    people_list.remove(drop_man)
