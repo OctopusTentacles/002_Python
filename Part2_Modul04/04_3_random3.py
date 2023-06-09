e384f3037f620ed26e47433114227c61e4bd9e3e
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-09 14:13:18 +0500

    Battle squad / random

--- Part2_Modul04/04_3_random3.py
+++ Part2_Modul04/04_3_random3.py
@@ -19,7 +19,9 @@ import random
 
 team_1 = [random.randint(50, 80) for _ in range(10)]
 team_2 = [random.randint(30, 60) for _ in range(10)]
-team_3 = []
+team_3 = [('Погиб' if team_1[i] + team_2[i] > 100 
+           else 'Выжил') 
+           for i in range(10)]
 
 print('Урон первого отряда:', team_1)
 print('Урон второго отряда:', team_2)
