b1a7406ad6a5e8c3322a00c868ab2e45efb8421b
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-10 15:25:55 +0500

    String like list / slices

--- Part2_Modul04/04_5_index&slices1.py
+++ Part2_Modul04/04_5_index&slices1.py
@@ -2,14 +2,14 @@
 
 word = 'Привет'
 
-sym_list = list(word)
+# sym_list = list(word)
 
-first_part = word[:len(sym_list) // 2]
-first_part = first_part[::-1]
-print(first_part)
+first_part = word[:len(word) // 2]
+# first_part = first_part[::-1]
+print(first_part[::-1])
 
-second_part = word[len(sym_list) // 2:]
-second_part = second_part[::-1]
-print(second_part)
+second_part = word[len(word) // 2:]
+# second_part = second_part[::-1]
+print(second_part[::-1])
 
-print(first_part + second_part)
+print(first_part[::-1] + second_part[::-1])
