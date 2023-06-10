51b0e6b70be13d04b6171f24508048ba4dfde6d5
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-10 15:22:15 +0500

    String like list / slices

--- Part2_Modul04/04_5_index&slices1.py
+++ Part2_Modul04/04_5_index&slices1.py
@@ -1,18 +1,15 @@
 # Задача Деление строки
 
 word = 'Привет'
-result = []
 
 sym_list = list(word)
 
-first_part = sym_list[:len(sym_list) // 2]
+first_part = word[:len(sym_list) // 2]
 first_part = first_part[::-1]
 print(first_part)
 
-second_part = sym_list[len(sym_list) // 2:]
+second_part = word[len(sym_list) // 2:]
 second_part = second_part[::-1]
 print(second_part)
 
-result.extend(first_part)
-result.extend(second_part)
-print(result)
+print(first_part + second_part)
