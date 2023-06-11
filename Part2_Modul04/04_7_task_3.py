81faf14353c9fd9c7858b3994f6844bff0be66bf
Octopus <klinov.artem@gmail.com>
2023-06-11 22:28:46 +0500

    Random uniform

--- Part2_Modul04/04_7_task_3.py
+++ Part2_Modul04/04_7_task_3.py
@@ -21,7 +21,9 @@
 import random
 
 team_1 = [round(random.uniform(5, 10), 2) for _ in range(20)]
-team_2 = []
+team_2 = [round(random.uniform(5, 10), 2) for _ in range(20)]
 winner = []
 
 print('Первая команда:', team_1)
+print('Вторая команда:', team_2)
+print('Победители тура:', winner)
