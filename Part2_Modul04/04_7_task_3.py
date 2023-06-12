34e0e146aed511746124635aa63a847cd29717d5
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-12 15:28:38 +0500

    Competitions / comprehensions / random  uniform

--- Part2_Modul04/04_7_task_3.py
+++ Part2_Modul04/04_7_task_3.py
@@ -22,7 +22,9 @@ import random
 
 team_1 = [round(random.uniform(5, 10), 2) for _ in range(20)]
 team_2 = [round(random.uniform(5, 10), 2) for _ in range(20)]
-winner = []
+winner = [team_1[index] 
+          if team_1[index] > team_2[index] 
+            else team_2[index] for index in range(20)]
 
 print('Первая команда:', team_1)
 print('Вторая команда:', team_2)
