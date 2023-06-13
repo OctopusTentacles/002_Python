071746e0c3ec7e336ab504609b4eace2830abd41
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-13 16:49:28 +0500

    Disclosure nested lists

--- Part2_Modul04/04_7_task_7.py
+++ Part2_Modul04/04_7_task_7.py
@@ -10,9 +10,13 @@
 
 nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[10, 11, 12], [13, 14, 15], [16, 17, 18]]]
 
+# nice = []
+# for i in nice_list:
+#     for j in i:
+#         for s in j:
+#             nice.append(s)
+# print(nice)
 
-
-# for i in range(len(nice_list)):
-#     for j in range(len(nice_list[i])):
-#         for s in range(len(nice_list[i][j])):
-#             print(nice_list[i][j][s], end=' ')
+print([s for i in nice_list
+                    for j in i
+                            for s in j])
