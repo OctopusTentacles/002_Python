968301f7f91cb7ad017cb7eaccb3108d8315ddd8
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-15 16:21:58 +0500

    draft

--- Part2_Modul05/05_2_f-strings3.py
+++ Part2_Modul05/05_2_f-strings3.py
@@ -10,3 +10,14 @@
 # на экран IP-адрес. Используйте переменную ip_address в качестве шаблона. 
 # Обеспечьте контроль ввода.
 
+
+ip_address = "{0}.{1}.{2}.{3}"
+
+count = 0
+numbers = []
+
+while count < 4:
+    new_number = int(input('Введите число: '))
+    if  0 <= new_number <= 255:
+        numbers.append(new_number)
+        count += 1
