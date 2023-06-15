8de55b9d80b6bb24d7975a1c3ef4e14109776766
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-15 16:52:57 +0500

    Pattern and ' * '

--- Part2_Modul05/05_2_f-strings3.py
+++ Part2_Modul05/05_2_f-strings3.py
@@ -21,3 +21,11 @@ while count < 4:
     if  0 <= new_number <= 255:
         numbers.append(new_number)
         count += 1
+    else:
+        print('Повторите ввод')
+
+print(ip_address.format(*numbers))
+
+# * полезный инструмент, но и без него можно справиться, 
+# вручную прописав элементы по индексам
+# print(ip_address.format(numbers[0], numbers[1], numbers[2], numbers[3]))
