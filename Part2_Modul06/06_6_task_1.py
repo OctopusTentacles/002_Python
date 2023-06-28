7d4609b2f323035bf01b424cef3af11e543f6ff5
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-28 13:23:34 +0500

    06_6_1 com2 draft

--- Part2_Modul06/06_6_task_1.py
+++ Part2_Modul06/06_6_task_1.py
@@ -15,3 +15,39 @@
 # Название третьей песни: Clean
 # Общее время звучания песен: 14,93 минуты
 
+
+num_dict = {
+    '1' : 'первой',
+    '2' : 'второй',
+    '3' : 'третьей',
+    '4' : 'четвертой',
+    '5' : 'пятой',
+    '6' : 'шестой',
+    '7' : 'седьмой',
+    '8' : 'восьмой',
+    '9' : 'девятой',
+}
+
+violator_songs = {
+    'World in My Eyes': 4.86,
+    'Sweetest Perfection': 4.43,
+    'Personal Jesus': 4.56,
+    'Halo': 4.9,
+    'Waiting for the Night': 6.07,
+    'Enjoy the Silence': 4.20,
+    'Policy of Truth': 4.76,
+    'Blue Dress': 4.29,
+    'Clean': 5.83
+}
+
+my_playlist = dict()
+
+amt = int(input('Сколько песен выбрать? '))
+
+for i_amt in range(1, amt + 1):
+    print(f'Название {num_dict[i_amt]} песни: ', end=' ')
+    song = input('')
+    if song in violator_songs:
+        my_playlist.add(violator_songs[song])
+
+print(my_playlist)
