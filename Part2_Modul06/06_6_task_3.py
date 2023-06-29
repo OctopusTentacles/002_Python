1a95fab63f783098d05bc1e076455c4cea193cbd
Octopus Tentacles <klinov.artem@gmail.com>
2023-06-29 17:23:45 +0500

    06_6_3 com2 draft

--- Part2_Modul06/06_6_task_3.py
+++ Part2_Modul06/06_6_task_3.py
@@ -17,27 +17,24 @@
 # Стул — 105 штук, стоимость 10 311 рублей.
 
 
-goods = {
-'Лампа': '12345',
-'Стол': '23456',
-'Диван': '34567',
-'Стул': '45678',
+goods = {'Лампа': '12345', 
+         'Стол': '23456', 
+         'Диван': '34567', 
+         'Стул': '45678',
 }
-store = {
-'12345': [
-{'quantity': 27, 'price': 42},
-],
-'23456': [
-{'quantity': 22, 'price': 510},
-{'quantity': 32, 'price': 520},
-],
-'34567': [
-{'quantity': 2, 'price': 1200},
-{'quantity': 1, 'price': 1150},
-],
-'45678': [
-{'quantity': 50, 'price': 100},
-{'quantity': 12, 'price': 95},
-{'quantity': 43, 'price': 97},
-],
+
+store = {'12345': [{'quantity': 27, 'price': 42},],
+         '23456': [{'quantity': 22, 'price': 510},
+                   {'quantity': 32, 'price': 520},],
+         '34567': [{'quantity': 2, 'price': 1200},
+                   {'quantity': 1, 'price': 1150},],
+         '45678': [{'quantity': 50, 'price': 100},
+                   {'quantity': 12, 'price': 95},
+                   {'quantity': 43, 'price': 97},],
 }
+
+print(store.keys())
+
+for i_goods in goods:
+    for i_store in store.keys(goods[i_goods]):
+        print(i_goods, i_store)
