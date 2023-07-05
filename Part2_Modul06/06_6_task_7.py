cb9c903304588ce7959f2859bbee50e1f82e12d7
Octopus <klinov.artem@gmail.com>
2023-07-05 21:09:52 +0600

    06_6_7 com3 * set

--- Part2_Modul06/06_6_task_7.py
+++ Part2_Modul06/06_6_task_7.py
@@ -23,13 +23,20 @@
 # Решение с множествами: 1
 
 
-array_1 = [1, 5, 10, 20, 40, 80, 100] 
-array_2 = [6, 7, 20, 80, 100] 
-array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
+array_1 = [1, 2, 3, 4] 
+array_2 = [2, 4] 
+array_3 = [2, 3]
+
+# array_1 = [1, 5, 10, 20, 40, 80, 100] 
+# array_2 = [6, 7, 20, 80, 100] 
+# array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
 
 print('Задача 1:')
+print('Решение без множеств:', )
+print('Решение с множествами:', *set(array_1).intersection(array_2, array_3))
 
-print(set.difference(array_1, array_2, array_3))
 
 
-print('Задача 2:')
+print('\nЗадача 2:')
+print('Решение без множеств:', )
+print('Решение с множествами:', *set(array_1).difference(array_2, array_3))
