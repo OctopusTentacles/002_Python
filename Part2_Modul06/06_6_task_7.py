e967eaee5d445dbd641b78a1a1f729c5e4ecdb93
Octopus <klinov.artem@gmail.com>
2023-07-05 20:44:53 +0600

    06_6_7 com2 draft

--- Part2_Modul06/06_6_task_7.py
+++ Part2_Modul06/06_6_task_7.py
@@ -26,3 +26,10 @@
 array_1 = [1, 5, 10, 20, 40, 80, 100] 
 array_2 = [6, 7, 20, 80, 100] 
 array_3 = [3, 4, 15, 20, 30, 70, 80, 120]
+
+print('Задача 1:')
+
+print(set.difference(array_1, array_2, array_3))
+
+
+print('Задача 2:')
