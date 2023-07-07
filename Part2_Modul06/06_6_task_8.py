21a57faa613728dada58925455d1c9cc24a5bdae
Octopus <klinov.artem@gmail.com>
2023-07-07 21:51:15 +0600

    06_6_8 com8 Palindrom again  / shifting symbol

--- Part2_Modul06/06_6_task_8.py
+++ Part2_Modul06/06_6_task_8.py
@@ -15,28 +15,45 @@
 # Основной функционал описан в отдельной функции(-ях)
 
 
-def shifting(shift_text):
-    shift_text = shift_text[1:] + shift_text[0]
-    palindron(shift_text)
+
+# def shifting(shift_text):
+#     shift_text = shift_text[1:] + shift_text[0]
+#     palindron(shift_text)
 
 
-def palindron(text):
-    for index in range(len(text)//2):
-        if text[index] != text[-1 - index]:
-            shifting(text)
-            break
-        elif text[index] == len(text)//2:
-            return True
-        elif text[index] == text[-1 - index]:
-            continue
+# def palindron(text):
+#     for index in range(len(text)//2):
         
-        else: 
-            return False
+#         if text[index] != text[-1 - index]:
+#             shifting(text)
+#         elif index + 1 == len(text)//2:
+#             return True
+#         elif text[index] == text[-1 - index]:
+#             continue
+#         else: 
+#             return False
         
+# word = (input('Введите слово: '))
+# if palindron(word):
+#     print('Можно сделать палиндромом')
+# print('Нельзя сделать палиндромом')
+
+
+# НЕ ПОЛУЧАЕТСЯ, Я ПОНИМАЮ ЧТО МЕНЯ ВОЗВРАЩАЕТ В SHIFTING И 
+# ВСЕ ЗАЦИКЛИНО И НЕ ЗНАЮ КАК ВЫЙТИ
+
+# план Б__________________________________________________________
 
 word = (input('Введите слово: '))
-if palindron(word):
-    print('Можно сделать палиндромом')
-print('Нельзя сделать палиндромом')
+count = 0
 
-
+while True:
+    if word == word[::-1]:
+        print('Можно сделать палиндромом')
+        break
+    elif count != len(word):
+        word = word[1:] + word[0]
+        count += 1
+    else:
+        print('Нельзя сделать палиндромом')
+        break
