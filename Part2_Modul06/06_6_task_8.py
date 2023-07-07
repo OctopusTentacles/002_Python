cf5125083859bdac3d368e5511735a3303702058
Octopus <klinov.artem@gmail.com>
2023-07-07 20:46:48 +0600

    06_6_8 com6 Palindrom again  / shifting symbol

--- Part2_Modul06/06_6_task_8.py
+++ Part2_Modul06/06_6_task_8.py
@@ -15,22 +15,23 @@
 # Основной функционал описан в отдельной функции(-ях)
 
 
+def shifting(shift_text):
+    shift_text = shift_text[1:] + shift_text[0]
+    palindron(shift_text)
+
+
 def palindron(text):
     for index in range(len(text)//2):
         if text[index] != text[-1 - index]:
             print(text[index], text[-1 - index])
             shifting(text)
     else:
-        print('Можно сделать палиндромом')
+        return True
         
 
-
-def shifting(shift_text):
-    shift_text = shift_text[1:] + shift_text[0]
-    palindron(shift_text)
-
-
 word = (input('Введите слово: '))
-palindron(word)
+if palindron(word):
+    print('Можно сделать палиндромом')
+print('Нельзя сделать палиндромом')
 
 
