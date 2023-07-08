75a8a09f312bb31f34755212fdbda9bbf53cfd51
Octopus <klinov.artem@gmail.com>
2023-07-08 13:50:07 +0600

    07_1_1 com3 palindrom again / get /

--- Part2_Modul07/07_1_palindrom.py
+++ Part2_Modul07/07_1_palindrom.py
@@ -2,9 +2,16 @@
 # Palindrom
 
 
-def is_poly(string):
-    
+def palindron(string):
+    char_dict = {}
+    for i_sym in string:
+        char_dict[i_sym] = char_dict.get(i_sym, 0) + 1
 
+    odd_count = 0
+    for i_value in char_dict.values():
+        if i_value % 2 != 0:
+            odd_count += 1
+    return odd_count <= 1
 
 word = (input('Введите слово: '))
 
