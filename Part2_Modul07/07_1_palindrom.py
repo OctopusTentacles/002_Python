b99a01d0671a295ed28436d135938f89648527ff
Octopus <klinov.artem@gmail.com>
2023-07-08 12:41:29 +0600

    07_1_1 com3 palindrom again /

--- Part2_Modul07/07_1_palindrom.py
+++ Part2_Modul07/07_1_palindrom.py
@@ -1,3 +1,14 @@
 # 7.1 Разбор домашнего задания
 # Palindrom
 
+
+def is_poly(string):
+    
+
+
+word = (input('Введите слово: '))
+
+if palindron(word):     
+    print('Можно сделать палиндромом')
+else:
+    print('Нельзя сделать палиндромом')
