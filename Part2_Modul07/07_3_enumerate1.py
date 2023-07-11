3a98a0baaf071439373b971e6d9b8ad014dd4856
Octopus <klinov.artem@gmail.com>
2023-07-11 21:30:11 +0500

    07_3_1 com4 enumerate def get_indexes

--- Part2_Modul07/07_3_enumerate1.py
+++ Part2_Modul07/07_3_enumerate1.py
@@ -17,3 +17,12 @@ word = input('Строка: ')
 i_index = [index for index, i_value in enumerate(word) if i_value == '~']
 
 print('Ответ:', *i_index)
+
+# ___________________________________________________________________________
+
+def get_indexes(where_to_search, what_to_search):
+    return [str(index) for index, letter in enumerate(where_to_search) if letter == what_to_search]
+
+
+text = input("Введите текст: ")
+print("Ответ:", " ".join(get_indexes(text, "~")))
