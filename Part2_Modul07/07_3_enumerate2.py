df970385ff938d853282f426d91e61f68cc161db
Octopus <klinov.artem@gmail.com>
2023-07-12 23:19:49 +0500

    07_3_2 com2 draft

--- Part2_Modul07/07_3_enumerate2.py
+++ Part2_Modul07/07_3_enumerate2.py
@@ -11,5 +11,12 @@
 # Первый словарь: {0: 'й', 1: 'р', 2: 'с', 3: 'г', 4: 'а', 5: 'а', 6: 'т', 7: 'ж', 8: 'е', 9: 'к'}
 # Второй словарь: {0: 'д', 1: 'а', 2: 'а', 3: 'в', 4: 'т', 5: 'ж', 6: 'р', 7: 'б', 8: 'й', 9: 'р'}
 
-import random
 
+import random
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+list_1 = [random.choice(alphabet) for _ in range(10)]
+list_2 = [random.choice(alphabet) for _ in range(10)]
+
+print(list_1)
+print(list_2)
