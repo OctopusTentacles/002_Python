69220db2ae2ba53e0221b89f769eb27af6a4dac5
Octopus <klinov.artem@gmail.com>
2023-07-13 22:19:10 +0500

    07_3_3 com2 draft

--- Part2_Modul07/07_3_enumerate3.py
+++ Part2_Modul07/07_3_enumerate3.py
@@ -19,3 +19,11 @@
 # если элемент принадлежит к этому типу данных, и возвращает False 
 # в противном случае.
 
+def return_even_elements(data):
+    
+
+
+print(return_even_elements('О Дивный Новый мир!'))
+print(return_even_elements([100, 200, 300, 'буква', 0, 2, 'а']))
+print(return_even_elements({0: 'е', 1: 'о', 2: 'ч', 3: 'ы', 4: 'в', 5: 'н', 
+                            6: 'д', 7: 'а', 8: 'ш', 9: 'ц'}))
