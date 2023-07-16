67a9fb6d3ea47d4a03df0abe0871b85b87639f26
Octopus <klinov.artem@gmail.com>
2023-07-16 12:49:28 +0500

    07_6_zip

--- Part2_Modul07/07_6_zip.py
+++ Part2_Modul07/07_6_zip.py
@@ -7,3 +7,6 @@ for i_person in people:
 
 print('\n', list(zip(names, ages)))   # список кортежей
 print(dict(zip(names, ages)))   # словарь из 2х списков
+
+print({i_names: i_ages + 10 
+       for i_names, i_ages in zip(names, ages)})   # comprehansions + 10 years
