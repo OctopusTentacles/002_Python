0daa398ab4390b5b3cb3f98182332964a1e65aed
Octopus <klinov.artem@gmail.com>
2023-07-18 22:53:15 +0500

    07_8_com2 hash / rabin_karp_search

--- Part2_Modul07/07_8_tuple_hash.py
+++ Part2_Modul07/07_8_tuple_hash.py
@@ -16,3 +16,35 @@ print(hash(tuple)) # Его хеш при каждом запуске может
 hash_dict = {(1, 2, 3): 'hello'} # Если я захочу создать словарь с этим кортежем,
 # то «под капотом» будет создан массив, в котором по индексу 529344067295497451 
 # будет храниться пара (1, 2, 3) и 'hello'
+
+
+# Алгоритм Рабина — Карпа
+
+def rabin_karp_search(text, pattern):
+    # Проверяем случаи, когда текст или подстрока пустые
+    if not text or not pattern:
+        return []
+
+    # Вычисляем хеш-значение для подстроки и первого окна текста
+    pattern_hash = hash(pattern)
+    window_hash = hash(text[:len(pattern)])
+
+    matches = [] # Список индексов совпадений
+
+    # Проходим по тексту с помощью скользящего окна
+    for i in range(len(text) - len(pattern) + 1):
+        # Если хеш-значения совпадают, сравниваем каждый символ окна с подстрокой
+        if pattern_hash == window_hash and text[i:i + len(pattern)] == pattern:
+            # Стоит уточнить, что благодаря ленивому выполнению Python, если первое условие в связке AND вернёт False, то второе не будет запускаться вообще
+            matches.append(i)
+
+        # Обновляем хеш-значение для следующего окна
+        window_hash = hash(text[i + 1:i + len(pattern) + 1])
+
+    return matches
+
+# Пример использования
+text = "abracadabra"
+pattern = "cad"
+matches = rabin_karp_search(text, pattern)
+print(f"Совпадения найдены на позициях: {matches}")
