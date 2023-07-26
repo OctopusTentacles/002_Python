1607b28900b3dc7ce7c0f29ca7bb6f1619fd5802
Octopus <klinov.artem@gmail.com>
2023-07-27 00:15:09 +0500

    08_1_ com3 min(len()) generator object

--- Part2_Modul08/08_1_my_zip.py
+++ Part2_Modul08/08_1_my_zip.py
@@ -6,5 +6,6 @@ nums_tpl = (10, 20, 30, 40)
 
 pairs = ((syms_str[i_elem], nums_tpl[i_elem]) 
          for i_elem in range(shortest_seq_range(syms_str, nums_tpl)))
-
 print(pairs)
+for i_elem in pairs:
+    print(i_elem)
