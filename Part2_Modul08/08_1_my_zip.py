082e1f5dfdcb040a74fe3057769adcd071eb906f
Octopus <klinov.artem@gmail.com>
2023-07-27 00:11:03 +0500

    08_1_ com2 min / len

--- Part2_Modul08/08_1_my_zip.py
+++ Part2_Modul08/08_1_my_zip.py
@@ -1,6 +1,10 @@
+def shortest_seq_range(string, tpl):
+    return min(len(string), len(tpl))
+
 syms_str = "abc"
 nums_tpl = (10, 20, 30, 40)
 
-pairs = [(syms_str[i_elem], nums_tpl[i_elem]) 
-         for i_elem in range(len(syms_str))]
+pairs = ((syms_str[i_elem], nums_tpl[i_elem]) 
+         for i_elem in range(shortest_seq_range(syms_str, nums_tpl)))
 
+print(pairs)
