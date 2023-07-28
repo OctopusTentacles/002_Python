9dff6ebcb9d1b3cde6f3022866c5d47e2d246ec4
Octopus <klinov.artem@gmail.com>
2023-07-28 22:42:02 +0600

    08_2_ 1_com4 recursion / factorial

--- Part2_Modul08/08_2_recursion1.py
+++ Part2_Modul08/08_2_recursion1.py
@@ -14,10 +14,11 @@
 
 
 def factorial(num):
-
+    if num == 1:
+        return 1
     fact_n_minus_1 = factorial(num -1)
     return num * fact_n_minus_1
 
 
-num_fact = factorial(1000)
+num_fact = factorial(5)
 print(num_fact)
