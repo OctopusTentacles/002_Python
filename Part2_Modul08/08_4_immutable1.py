3c08d82b674e300ef60c470281a2d2a7c20f05b6
Octopus <klinov.artem@gmail.com>
2023-08-01 20:42:08 +0500

    08_4_1_com5 copy() /

--- Part2_Modul08/08_4_immutable1.py
+++ Part2_Modul08/08_4_immutable1.py
@@ -38,7 +38,13 @@ uniq_nums = {1, 2, 3}
 common_dict = {1: nums_list.copy(), 2: some_dict.copy(), 3: uniq_nums.copy(), 4: (10, 20, 30)}
 change_dict(common_dict)
 
-
 print(common_dict)
-
 print(nums_list, some_dict, uniq_nums)
+
+# Либо мы можем применить вспомогательную функцию, которая сделает это за нас:
+
+import copy
+
+common_dict = {1: nums_list, 2: some_dict, 3: uniq_nums, 4: (10, 20, 30)}
+common_dict_2 = copy.deepcopy(common_dict)
+# Она будет особенно полезна в структурах, в которых множество вложенных переменных
