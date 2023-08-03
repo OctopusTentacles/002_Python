a994ede7856365fde06138d57fbf52f1e309a752
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-03 17:17:59 +0500

    08_6_3_com2 draft

--- Part2_Modul08/08_6_named_argum3.py
+++ Part2_Modul08/08_6_named_argum3.py
@@ -15,23 +15,29 @@
 # Нужна ваша помощь, вот сама программа:
 
 
-def create_dict(data, template=dict()):
+def create_dict(data, template=None):
     if isinstance(data, dict):
         return data
 
-    if isinstance(data, int) or isinstance(data, float) or isinstance(data, str):
-        return template[data] = data
+    elif isinstance(data, (int, float, str)):
+        template = template or dict()
+        template = template[data] = data
+        return template
+    else:
+        return None
 
  
 
 def data_preparation(old_list):
     new_list = []
     for i_element in old_list:
-        new_list.append(create_dict(i_element))
+        new_element = create_dict(i_element)
+        if new_element:
+            new_list.append(new_element)
     return new_list
 
 
-data = [“sad”, {“sds”: 23}, {43}, [12, 42, 1], 2323]
+data = ['sad', {'sds': 23}, {43}, [12, 42, 1], 2323]
 
 data = data_preparation(data)
 
