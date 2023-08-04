f74096b2e88bdfe59bb68721480ae5c40b4cc259
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-04 10:10:21 +0500

    08_7_com2 draft

--- Part2_Modul08/08_7_args_kwargs.py
+++ Part2_Modul08/08_7_args_kwargs.py
@@ -1,8 +1,12 @@
 # подсчет суммы налога
 
-def print_tax_document():
+def print_tax_document(tax, *args): # *-неизвестное количество
+    price_sum = 0
+    for i in args:
+        price_sum = price_sum + i * tax / 100
+    print('Сумма налога: ', price_sum)
 
-    
+
 
 my_tax = int(input('Величина налога: '))
 print_tax_document(my_tax, 1000, 950, 880, 920, 990) #налог и цены на товары
