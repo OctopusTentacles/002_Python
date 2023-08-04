45525a6706a00070722fd773ea1e98223841b33a
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-04 14:09:52 +0500

    08_7_com4 *args, **kwargs

--- Part2_Modul08/08_7_args_kwargs.py
+++ Part2_Modul08/08_7_args_kwargs.py
@@ -1,15 +1,18 @@
 # подсчет суммы налога
 
-def print_tax_document(tax, *args): # *-неизвестное количество
+def print_tax_document(tax, *args, **kwargs): # *-неизвестное количество
     price_sum = 0
     for i in args:
         price_sum = price_sum + i * tax / 100
     print('Сумма налога: ', price_sum)
 
+    for i_info, i_value in kwargs.items(): #имен. арг. передаются как словарь(имя=значение)
+       print('{}: {}'.format(i_info, i_value)) 
+
 
 
 my_tax = int(input('Величина налога: '))
-print_tax_document(my_tax, 1000, 950, 880, 920, 990,    # налог и цены на товары
+print_tax_document(my_tax, 1000, 950, 880, 920, 990,  #налог и цены на товары (позиционные арг)
                    year=1997, 
                    doc_type='Report', 
                    operation_id=1110034)            # именнованные аргументы 
