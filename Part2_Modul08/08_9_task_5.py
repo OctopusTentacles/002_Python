f2fe0de8ce26603e0fdc4634d3b9cc6de6361443
Octopus <klinov.artem@gmail.com>
2023-08-08 22:14:06 +0500

    08_9_5_com4 expand list / slices

--- Part2_Modul08/08_9_task_5.py
+++ Part2_Modul08/08_9_task_5.py
@@ -25,8 +25,8 @@ def expand_list(data):
     if data == []:
         return data
     if isinstance(data[0], list):
-        return (expand_list(data[0]) + )
-
+        return (expand_list(data[0]) + expand_list(data[1:]))
+    return data[0] + expand_list(data[1:])
 
 nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]], 
              [[11, 12, 13], [14, 15], [16, 17, 18]]]
