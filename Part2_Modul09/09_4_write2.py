8c20d8fbf19156964c0cd684b3664826a7e4d175
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-14 17:11:48 +0500

    09_4_2_com2 draft

--- Part2_Modul09/09_4_write2.py
+++ Part2_Modul09/09_4_write2.py
@@ -8,4 +8,54 @@
 # Напишите программу, которая копирует код каждого скрипта в папке проекта 
 # python_basic в файл scripts.txt, разделяя код строкой из 40 символов *. 
 
+# Пример содержимого файла scripts.txt:
+
+# import platform
+# import sys
+
+# info = 'OS info is \n{}\n\nPython version is {} {}'.format(
+#     platform.uname(),
+#     sys.version,
+#     platform.architecture(),
+# )
+
+# print(info)
+
+# with open('os_info.txt', 'w', encoding='utf8') as file:
+#     file.write(info)
+
+# ****************************************
+
+# print("Введите первую точку")
+# x1 = float(input('X: '))
+# y1 = float(input('Y: '))
+# print("\nВведите вторую точку")
+# x2 = float(input('X: '))
+# y2 = float(input('Y: '))
+ 
+# print("Уравнение прямой, проходящей через эти точки:")
+# x_diff = x1 - x2
+# y_diff = y1 - y2
+# if x_diff == 0:
+#     print("x = ", x1)
+# elif y_diff == 0:
+#     print("y = ", y1)
+# else:
+#     k = y_diff / x_diff
+#     b = y2 - k * x2
+#     print("y = ", k, " * x + ", b)
+
+# ****************************************
+
+
+
 import os
+
+def find_file(cur_path, ending):
+    all_paths = list()
+
+    for i_elem in os.listdir(cur_path):
+        
+
+
+all_py_files = find_file('..', '.py')
