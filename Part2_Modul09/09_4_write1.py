532e413979743ab61c11d74bbe86f57c8090b5cf
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-14 14:19:12 +0500

    09_4_1_com2 draft

--- Part2_Modul09/09_4_write1.py
+++ Part2_Modul09/09_4_write1.py
@@ -14,3 +14,17 @@
 # Содержимое файла answer.txt
 # 20
 
+import os
+
+def find_path(cur_path, name):
+    for i_elem in os.listdir(cur_path):
+        search_path = os.path.join(cur_path, i_elem)
+        if i_elem == name:
+            return search_path
+
+
+path = find_path('', 'Part2_Modul09')
+
+print(path)
+
+# numbers_file = open('numbers.txt', 'w')
