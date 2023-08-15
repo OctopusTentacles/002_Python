d6a7a1b8f0b3576efdb9940d7655863c04c197d3
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-15 17:06:18 +0500

    09_5_com2 seek

--- Part2_Modul09/09_5_seek.py
+++ Part2_Modul09/09_5_seek.py
@@ -1,2 +1,23 @@
 # 9.5 Перемещение курсора в файле. Метод seek
 
+import os
+
+path = os.getcwd()
+
+speakers_file = open(os.path.join(path,'Part2_Modul09\speakers.txt'), 'r', encoding='utf-8')
+data = speakers_file.read(12)
+data2 = speakers_file.read(8)
+# read(20) перемещает курсор по файлу, 
+
+print(data)
+print(data2)
+
+
+speakers_file.seek(1)
+data2 = speakers_file.read(5)
+# seek ставит курсор на позицию, НО в файле должна быть
+# латиница, с кириллицей будет ощибка
+
+print(data2)
+
+speakers_file.close()
