5b6f411e426dde4db76b1285195e70711b1f37ef
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-16 10:15:28 +0500

    09_6_1_com3 w&r file

--- Part2_Modul09/09_6_task_1.py
+++ Part2_Modul09/09_6_task_1.py
@@ -33,7 +33,20 @@ way = os.path.abspath(find_dir('..', 'Part2_Modul09'))
 # что бы относящиеся друг к другу файлы были в одном месте
 
 if way:
+    numbers_sum = 0
 
+    numbers_file = open(os.path.join(way, 'numbers.txt'), 'w', encoding='utf8')
+    numbers_file.write(str('     2 \n2 \n   2 \n         2'))
+    numbers_file.close()
+    # Обязательно закрывать файл? Вроде работает без закрытия, но я его открываю
+    # еще раз для чтения - это правильно? Иначе выходят ошибки:
+    # ValueError: Must have exactly one of create/read/write/append mode and at most one plus
+    # io.UnsupportedOperation: not readable
+    
+    numbers_file = open(os.path.join(way, 'numbers.txt'), 'r', encoding='utf8')
+    for line in numbers_file:
+        numbers_sum += int(line.strip())
+    numbers_file.close()
 
 
 else:
