46cef937b0a975628ae3e1be378917673283d8b9
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-16 17:10:47 +0500

    09_6_2_com2 Zen Python vice versa

--- Part2_Modul09/09_6_task_2.py
+++ Part2_Modul09/09_6_task_2.py
@@ -14,4 +14,27 @@
 
 import os
 
-open('zen.txt', 'r', encoding='utf8')
+def find_dir(cur_path, name_file):
+    for i_dir in os.listdir(cur_path):
+        find_path = os.path.join(cur_path, i_dir)
+        if i_dir == name_file:
+            return os.path.abspath(find_path)
+        elif os.path.isdir(find_path):
+            result = find_dir(find_path, name_file)
+            if result:
+                return result
+
+
+path = find_dir('..', 'zen.txt')
+# у меня для нахождения пути к файлу или директории
+# всегда используется код выше - одна и та же функция.
+# это нормально(правильно)???
+
+# в гугле нашел такую штуку:
+# dir = os.path.dirname(__file__)
+# дает путь в директорию скрипта, 
+# но мы такие черточки))))) не проходили, поэтому оставил def
+
+zen_file = open(path, 'r', encoding='utf8')
+zen_list = [i_line for i_line in zen_file]
+print(*zen_list[::-1])
