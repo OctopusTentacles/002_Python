345597837e2f869759b1b92ee82bba7308234261
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-16 15:08:35 +0500

    09_6_1_com5  num file to sum file

--- Part2_Modul09/09_6_task_1.py
+++ Part2_Modul09/09_6_task_1.py
@@ -28,15 +28,16 @@ def find_dir(cur_puth, file):
                 return result
 
 
-way = os.path.abspath(find_dir('..', 'Part2_Modul09'))
+way = find_dir('..', 'Part2_Modul09')
 # для работы в определенной директории, 
 # это не по ТЗ, но мне так удобнее, 
 # что бы сопутствующие файлы были в одном месте
 
 if way:
+    path = os.path.abspath(way)
     numbers_sum = 0
 
-    numbers_file = open(os.path.join(way, 'numbers.txt'), 'w', encoding='utf8')
+    numbers_file = open(os.path.join(path, 'numbers.txt'), 'w', encoding='utf8')
     numbers_file.write(str('     2 \n2 \n   2 \n         2'))
     numbers_file.close()
     # Обязательно закрывать файл? Вроде работает без закрытия, но я его открываю
@@ -44,12 +45,12 @@ if way:
     # ValueError: Must have exactly one of create/read/write/append mode and at most one plus
     # io.UnsupportedOperation: not readable
     
-    numbers_file = open(os.path.join(way, 'numbers.txt'), 'r', encoding='utf8')
+    numbers_file = open(os.path.join(path, 'numbers.txt'), 'r', encoding='utf8')
     for line in numbers_file:
         numbers_sum += int(line.strip())
     numbers_file.close()
 
-    answer_file = open(os.path.join(way, 'answer.txt'), 'w', encoding='utf8')
+    answer_file = open(os.path.join(path, 'answer.txt'), 'w', encoding='utf8')
     answer_file.write(str(numbers_sum))
     answer_file.close()
 
@@ -57,4 +58,53 @@ if way:
 else:
     print('NO such Directory!')
 
+# ____________________________________________________________________________________
 
+def find_dir(cur_puth, file):
+    for i_dir in os.listdir(cur_puth):
+        find_path = os.path.join(cur_puth, i_dir)
+        if i_dir == file:
+            return find_path
+        elif os.path.isdir(find_path):
+            result = find_dir(find_path, file)
+            if result:
+                return result
+
+
+def write_file(path_file, name_file, content):
+    redact_file = open(os.path.join(path_file, name_file), 'w', encoding='utf8')
+    redact_file.write(str(content))
+    redact_file.close()
+    return redact_file
+
+
+def read_file_sum(path_file, name_file):
+    content_sum = 0
+    redact_file = open(os.path.join(path_file, name_file), 'r', encoding='utf8')
+    for i_line in redact_file:
+        content_sum += int(i_line.strip())
+    redact_file.close()
+    return content_sum
+
+
+
+way = find_dir('..', 'Part2_Modul09')
+
+if way:
+    path = os.path.abspath(way)
+    
+    numbers_file = write_file(path, 
+                              'numbers.txt', 
+                              '     2 \n2 \n   2 \n         2')
+    
+    numbers_sum = read_file_sum(path, 'numbers.txt',)
+
+    answer_file = write_file(path, 
+                              'answer.txt', 
+                              numbers_sum)
+
+
+else:
+    print('NO such Directory!')
+
+# Добавил функции, не знаю как лучше, но оба варианта работают
