527c97ccf480ee16f8216d248036f3cad6982cc7
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-20 13:21:12 +0500

    09_6_6_com3 chr / ord

--- Part2_Modul09/09_6_task_6.py
+++ Part2_Modul09/09_6_task_6.py
@@ -16,12 +16,24 @@ import os
 import zipfile
 
 
-def read_file():
+def read_file(cur_dir, file_name):
+    with open(os.path.join(cur_dir, file_name), 'r') as read_file:
+        file_list = read_file.readlines()
+    print(file_list)
 
+
+# MAIN_CODE==========================================================
+# chr(1072 1103) - rus  chr(1040 1071) - RUS 
+# chr(97 122) - eng     chr(65 90) - ENG 
 
 
 current_directory = os.path.dirname(__file__)
 print(current_directory)
 
-with zipfile.ZipFile('voina-i-mir.zip', 'r') as zf:
-    zf.extractall('voina-i-mir.txt')
+
+# with zipfile.ZipFile('voina-i-mir.zip', 'r') as zf:
+#     zf.extractall('voina-i-mir.txt')
+
+
+read_file(current_directory, 'second_tour.txt')
+
