0bb805874af65ce63e19033196107c82c4a9844f
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-22 13:11:05 +0500

    10_1_com2 draw table

--- Part2_Modul10/10_1_home_task.py
+++ Part2_Modul10/10_1_home_task.py
@@ -9,8 +9,23 @@ def collect_stats(file_name):
     for i_line in text_file:
         for j_char in i_line:
             if j_char.isalpha():
+                if j_char not in result:
+                    result[j_char] = 0
+                result[j_char] += 1
+    text_file.close()
+    return result
+
+
+def print_stats(stats):
+    print('+{:-^19}+'.format('+')) 
+    #сделать 19 символов поставив знак '+' по центру, а края заполнить знком '-'
+    print('|{: ^9}|{: ^9}|'.format('буква', 'частота'))
+    print('+{:-^19}+'.format('+'))
+    for char, count in stats.items():
+        print('|{: ^9}|{: ^9}|'.format(char, count))
+    print('+{:-^19}+'.format('+'))
 
 
 file_name = 'voyna-i-mir.txt'
 stats = collect_stats(file_name)
-print(stats)
+print_stats(stats)
