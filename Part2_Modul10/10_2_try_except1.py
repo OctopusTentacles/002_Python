984929aaab26b3e857fe94d3e88b542949060fb6
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-22 17:27:53 +0500

    10_2_1_com3 try except / as exc

--- Part2_Modul10/10_2_try_except1.py
+++ Part2_Modul10/10_2_try_except1.py
@@ -17,14 +17,21 @@
 # Для каждого типа исключений выведите на консоль соответствующее сообщение.
 
 
-try:
-    BRUCE_WILLIS = 42
+BRUCE_WILLIS = 42
 
-    input_data = input('Введите строку: ')
+input_data = input('Введите строку: ')
+
+try:
     leeloo = int(input_data[4])
     result = BRUCE_WILLIS * leeloo
     print(f'- Leeloo Dallas! Multi-pass № {result}!')
-except ValueError:
-    print('невозможно преобразовать к числу')
-except IndexError:
-    print('выход за границы списка')
+
+except ValueError as exc:
+# as - этот оператор запишет пойманное исключение в переменную exc (название может быть любым)
+    print(type(exc),'- невозможно преобразовать к числу')
+
+except IndexError as exc:
+    print(type(exc),'- выход за границы списка')
+
+except Exception as exc:
+    print(type(exc),'- поймано исключение')
