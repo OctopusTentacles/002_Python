75c0fecd3ca966096d21dd29041dbf9d0bae0146
Octopus Tentacles <klinov.artem@gmail.com>
2023-08-25 16:47:45 +0500

    10_5_com2 with as / check closed

--- Part2_Modul10/10_5_with as.py
+++ Part2_Modul10/10_5_with as.py
@@ -5,11 +5,10 @@ line_count = 0
 total_sym = 0
     
 try:
-
-    with open(os.path.join(cur_dir, 'people.txt'), 'r', encoding='utf8') as open_file:
+    with open(os.path.join(cur_dir, 'people.txt'), 'r', encoding='utf8') as people_file:
     # open_file = open(os.path.join(cur_dir, 'people.txt'), 'r', encoding='utf8')
     
-        for i_line in open_file.readlines():
+        for i_line in people_file.readlines():
             line_count += 1
             sym_in_line = 0
             for i_sym in i_line:
@@ -23,10 +22,9 @@ try:
 
     # open_file.close()
         
-
 except FileNotFoundError as exc:
     print(type(exc), 'Нет такого файла или директории')    
     
-    
 finally:
     print('общее число символов:', total_sym)
+    print(people_file.closed)
