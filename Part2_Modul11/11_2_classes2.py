07f1644561c7efd375e11ecc8a9eba37b0fcd7fc
Octopus <klinov.artem@gmail.com>
2023-09-03 19:32:49 +0600

    11_2_2 com2 draft

--- Part2_Modul11/11_2_classes2.py
+++ Part2_Modul11/11_2_classes2.py
@@ -12,46 +12,33 @@
 # Поправьте программиста: перепишите код, используя классы и экземпляры классов.
 
 
-monitor_name_1 = 'Samsung'
-monitor_matrix_1 = 'VA'
-monitor_res_1 = 'WQHD'
-monitor_freq_1 = 60
+class Monitor:
+    name = 'Samsung'
+    matrix = 'VA'
+    resolution = 'WQHD'
+    frequency = 60
 
-monitor_name_2 = 'Samsung'
-monitor_matrix_2 = 'VA'
-monitor_res_2 = 'WQHD'
-monitor_freq_2 = 144
+monitor_1 = Monitor()
+monitor_2 = Monitor()
+monitor_3 = Monitor()
+monitor_4 = Monitor()
 
-monitor_name_3 = 'Samsung'
-monitor_matrix_3 = 'VA'
-monitor_res_3 = 'WQHD'
-monitor_freq_3 = 70
+monitor_1.frequency = 60
+monitor_2.frequency = 144
+monitor_3.frequency = 70
+monitor_4.frequency = 60
 
-monitor_name_4 = 'Samsung'
-monitor_matrix_4 = 'VA'
-monitor_res_4 = 'WQHD'
-monitor_freq_4 = 60
 
- 
+class Headphones:
+    name = 'Sony'
+    sensitivity = 108
+    microphone = False
 
-headphones_name_1 = 'Sony'
+headphones_1 = Headphones()
+headphones_2 = Headphones()
+headphones_3 = Headphones()
 
-headphones_sensitivity_1 = 108
-
-headphones_micro_1 = False
-
-headphones_name_2 = 'Sony'
-
-headphones_sensitivity_2 = 108
-
-headphones_micro_2 = True
-
-headphones_name_3 = 'Sony'
-
-headphones_sensitivity_3 = 108
-
-headphones_micro_3 = True
-
- 
-
+headphones_1.microphone = False
+headphones_2.microphone = True
+headphones_3.microphone = True
 
