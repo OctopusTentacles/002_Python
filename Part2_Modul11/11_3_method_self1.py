b768ddaf01fb2a5613865f951bdfd60cf2c46cb5
Octopus <klinov.artem@gmail.com>
2023-09-04 19:41:28 +0600

    11_3_1 com3 methods

--- Part2_Modul11/11_3_method_self1.py
+++ Part2_Modul11/11_3_method_self1.py
@@ -11,6 +11,8 @@
 # Метод, который позволяет устанавливать текущую скорость машины.
 # Проверьте работу этих методов.
 
+import random
+
 class Toyota:
     color = 'red'
     price = 1e6
@@ -21,7 +23,15 @@ class Toyota:
         print('color: {}\nprice: {}\nmax_speed: {}\n'
               'cur_speed: {}'.format(self.color, self.price,
                                      self.max_speed, self.cur_speed))
+    
+    def set_speed(self, speed):
+        self.cur_speed = speed
+        car_1.print_info()
         
 
 car_1 = Toyota()
 car_1.print_info()
+car_1.set_speed(random.randint(0, 200))
+print(Toyota.cur_speed)
+# обратите внимание, что скорость внутри Класса осталась той же, 
+# её изменения не коснулись
