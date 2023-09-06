79280e59e8652bb7c7acb1786ecf2e755b64766f
Octopus <klinov.artem@gmail.com>
2023-09-06 15:30:32 +0600

    11_4_3 com3 farm / Potato /

--- Part2_Modul11/11_4__init__3.py
+++ Part2_Modul11/11_4__init__3.py
@@ -44,11 +44,26 @@
 class Potato:
     states = {0: 'Отсутствует', 1: 'Росток', 2: 'Зеленая', 3: 'Зрелая'}
 
+    def __init__(self, index):
+        self.index = index
+        self.state = 0
 
+    def grow(self):
+        if self.state < 3:
+            self.state += 1
+        self.print_state()
 
+    def is_ripe(self):
+        if self.state == 3:
+            return True
+        return False
+
+    def print_state(self):
+        print('Картошка {} сечас {}'.format(
+            self.index, Potato.states[self.state]))
 
 
 class PotatoGarden:
 
     def __init__(self, count):
-        self.potatos = []
+        self.potatos = [Potato(index) for index in range(1, count + 1)]
