954129656994be12e7ff7750bd356763acd1f064
Octopus Tentacles <klinov.artem@gmail.com>
2023-10-02 10:05:29 +0500

    12_1_com2 class Water

--- Part2_Modul12/12_1_home_task.py
+++ Part2_Modul12/12_1_home_task.py
@@ -7,13 +7,50 @@
 #   Огонь + Земля = Лава
 
 class Water:
-    pass
+    def __str__(self):
+        return 'Вода'
+    
+    def __add__(self, other):
+        if isinstance(other, Air):
+            return Storm()
+        elif isinstance(other, Fire):
+            return Steam()
+        elif isinstance(other, Earth):
+            return Dirt()
+        else:
+            return None
+
 
 class Air:
-    pass
+    def __str__(self):
+        return 'Воздух'
+
+    def __add__(self, other):
+        if isinstance(other, Water):
+            return Storm()
+
 
 class Fire:
-    pass
+    def __str__(self):
+        return 'Огонь'
+
 
 class Earth:
-    pass
+    # TODO дописать класс
+    def __str__(self):
+        return 'Земля'
+
+
+class Storm:
+    def __str__(self):
+        return 'Шторм'
+        
+
+
+
+
+water = Water()
+air = Air()
+
+
+print(f'Вода + Воздух = {water + air}')
