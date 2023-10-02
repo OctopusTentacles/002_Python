5e0a3e3d0bf9de4d789d75caca2cfbc8e9772cb5
Octopus Tentacles <klinov.artem@gmail.com>
2023-10-02 14:53:48 +0500

    11_6_5_v.0.1_com2 def live_one_day

--- Part2_Modul11/11_6_task_5_v0.1.py
+++ Part2_Modul11/11_6_task_5_v0.1.py
@@ -12,3 +12,25 @@ class Person:
         self.name = name
         self.house = house
         self.satiety = 50
+
+    def eat(self):
+        if self.house.fridge > 0:
+            self.satiety += 10
+            self.house.fridge -= 10
+            print(f'{self.name} поел.')
+
+    def work(self):
+        self.satiety -= 10
+        self.house.money += 50
+        print(f'{self.name} поработал.')
+
+    def play(self):
+        self.satiety -= 10
+        print(f'{self.name} поиграл.')
+
+    def shop(self):
+        if self.house.money >= 10:
+            self.house.fridge += 20
+            self.house.money -= 20
+            print(f'{self.name} сходил в магазин.')
+
