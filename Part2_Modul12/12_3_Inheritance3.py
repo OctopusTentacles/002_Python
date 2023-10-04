9230e0ca17dd0be59e86f9f8b800212af524fc66
Octopus Tentacles <klinov.artem@gmail.com>
2023-10-04 16:30:27 +0500

    12_3_3_com2 My Own Exception

--- Part2_Modul12/12_3_Inheritance3.py
+++ Part2_Modul12/12_3_Inheritance3.py
@@ -18,10 +18,17 @@
 # Дополнительно, с помощью try except, можно обработать исключение на своё усмотрение.
 
 
-class DevisionError():
+class DivisionError(Exception):
     pass
+
+
 with open("numbers.txt", "r", encoding="utf8") as file:
     for line in file:
         try:
             clear_line = line.strip()
-            
+            first, second = clear_line.split()
+            if int(first) < int(second):
+                raise DivisionError("нельзя делить меньшее на большее")
+        except (ValueError, DivisionError) as exc:
+            print(exc, type(exc), first, second)
+
