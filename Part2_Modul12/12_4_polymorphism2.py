f0f96bbdfe87000cef82879c385c65a59a5a6ac7
Octopus Tentacles <klinov.artem@gmail.com>
2023-10-05 17:06:21 +0500

    12_4_1_com3 class CanFly

--- Part2_Modul12/12_4_polymorphism2.py
+++ Part2_Modul12/12_4_polymorphism2.py
@@ -27,25 +27,34 @@
 
 
 class CanFly:
-    def __init__(self, hight=0, speed=0):
-        self.hight = hight
-        self.speed = speed
+    def __init__(self):
+        self.hight = 0
+        self.speed = 0
 
-    def take_off(self, hight):
-        print(f"взлетаем на высоту {hight}")
+    def take_off(self):
+        pass
 
-    def to_fly(self, speed):
-        print(f"летим со скоростью {speed}")
+    def to_fly(self):
+        pass
 
     def landing(self):
         self.hight = 0
         self.speed = 0
         print(self.hight, self.speed)
 
+    def __str__(self):
+        return "{}: высота {}, скорость {}".format(
+            self.__class__.__name__, self.hight, self.speed)
+
 
 class Baterfly(CanFly):
-    pass
+    def __init__(self, hight, speed):
+        super().__init__(hight, speed)
 
 
 class Rocket(CanFly):
     pass
+
+
+baterfly = Baterfly(1, 0.5)
+baterfly.take_off(1)
