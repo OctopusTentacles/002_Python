68985dba50347125d661ca886323319f3e10dec7
Octopus Tentacles <klinov.artem@gmail.com>
2023-10-05 14:08:17 +0500

    12_4_1_com7 Units other

--- Part2_Modul12/12_4_polymorphism1.py
+++ Part2_Modul12/12_4_polymorphism1.py
@@ -61,3 +61,29 @@ citizen = Citizen(hitpoint=100)
 print(citizen.get_hitpoint())
 citizen.damage(damage=40)
 print(citizen)
+
+
+# class Unit:
+
+#     def __init__(self, hp):
+#         self.__hp = hp
+
+#     def set_hp(self, amount):
+#         self.__hp = amount
+
+#     def get_hp(self):
+#         return self.__hp
+
+#     def get_damage(self, amount):
+#         self.set_hp(self.get_hp() - 0)  # -0 написан для наглядности, в теории  мы могли бы этого и не писать
+
+
+# class Soldier(Unit):
+
+#     def get_damage(self, amount):
+#         self.set_hp(self.get_hp() - amount)
+
+
+# class Citizen(Unit):
+#     def get_damage(self, amount):
+#         self.set_hp(self.get_hp() - amount * 2)
