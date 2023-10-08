962aa88a8b1d664340e95dd5f4793400f941fa4f
Octopus <klinov.artem@gmail.com>
2023-10-08 16:47:13 +0500

    12_7_1_com2 draft

--- Part2_Modul12/12_7_task_1.py
+++ Part2_Modul12/12_7_task_1.py
@@ -22,15 +22,29 @@ class Property:
     def __init__(self, worth):
         self.worth = worth
 
+    def tax_calc(self):
+        pass
 
+    
 class Apartment(Property):
+    def __init__(self, worth):
+        super().__init__(worth)
 
+    def tax_calc(self):
+        return self.worth / 1000
 
 
 class Car(Property):
+    def __init__(self, worth):
+        super().__init__(worth)
 
-
+    def tax_calc(self):
+        return self.worth / 200
 
 
 class CountryHouse(Property):
-    
+    def __init__(self, worth):
+        super().__init__(worth)
+
+    def tax_calc(self):
+        return self.worth / 500
