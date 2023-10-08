7fce349d9e2eb0ce2926fd22b409a413f5548592
Octopus <klinov.artem@gmail.com>
2023-10-08 20:55:37 +0500

    12_7_1_com4 MAIN CODE

--- Part2_Modul12/12_7_task_1.py
+++ Part2_Modul12/12_7_task_1.py
@@ -22,10 +22,15 @@ class Property:
         self.worth = worth
 
     def tax_calc(self, tax):
-        print(f"Налог - {tax}")
+        print(f"Налог на {self.__class__.__name__} = {tax}")
+        if self.worth > tax:
+            self.worth -= tax
+            print(f"Ваш баланс: {self.worth}")
+        else:
+            print(f"Недостаточно средств!")
 
-    def __str__(self):
-        return f"Стоимость {self.__class__.__name__} {self.worth}"
+    # def __str__(self):
+    #     return f"Стоимость {self.__class__.__name__} {self.worth}"
 
     
 class Apartment(Property):    
@@ -49,11 +54,16 @@ class CountryHouse(Property):
         super().__init__(worth)
 
     def tax_calc(self):
+        super().tax_calc(self.worth / 500)
 
-        return f"Налог - {self.worth / 500}"
 
-
+"""MAIN CODE"""
 money = int(input("Сколько у вас денег? "))
-apartment = Apartment(int(input("Сколькь стоит квартира? ")))
-print(apartment)
+apartment = Apartment(int(input(f"цена квартиры: ")))
+car = Car(int(input("цена машины: ")))
+house = CountryHouse(int(input("цена дачи: ")))
+
+# print(apartment)
 apartment.tax_calc()
+car.tax_calc()
+house.tax_calc()
