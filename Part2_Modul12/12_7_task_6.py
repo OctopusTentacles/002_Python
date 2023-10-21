200f35885afc970d4fe6eec70929c04db2f4927a
Octopus Tentacles <klinov.artem@gmail.com>
2023-10-21 12:46:55 +0500

    12_7_6_com5 ABC Circle

--- Part2_Modul12/12_7_task_6.py
+++ Part2_Modul12/12_7_task_6.py
@@ -49,23 +49,27 @@ class Rectangle(Shape):
         return round(self.side_a * self.side_b)
 
 
-
 class Triangle(Shape):
-    pass
+    def __init__(self, side, height):
+        self.side = side
+        self.height = height
+
+    def area(self):
+        return round((self.side * self.height) / 2)
 
 
 # Примеры работы с классом:
 # Создание экземпляров классов
 circle = Circle(5)
 rectangle = Rectangle(4, 6)
-# triangle = Triangle(3, 8)
+triangle = Triangle(3, 8)
 
 # # Вычисление площади фигур
 circle_area = circle.area()
 rectangle_area = rectangle.area()
-# triangle_area = triangle.area()
+triangle_area = triangle.area()
 
 # # Вывод результатов
 print("Площадь круга:", circle_area)
 print("Площадь прямоугольника:", rectangle_area)
-# print("Площадь треугольника:", triangle_area)
+print("Площадь треугольника:", triangle_area)
