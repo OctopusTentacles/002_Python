7311a4ac8cea17f772dc53c3643fff500323e638
Octopus Tentacles <klinov.artem@gmail.com>
2023-10-21 16:30:11 +0500

    13_1_com2 __str__ / join

--- Part2_Modul13/13_1_home_task.py
+++ Part2_Modul13/13_1_home_task.py
@@ -6,7 +6,8 @@ class Stack:
         self.__st = []
 
     def __str__(self):
-        return str(self.__st)
+        return "; ".join(self.__st)
+        # return str(self.__st)
     
     def push(self, elem):
         self.__st.append(elem)
@@ -15,3 +16,35 @@ class Stack:
         if len(self.__st) == 0:
             return None
         return self.__st.pop()
+    
+
+class TaskManager:
+    def __init__(self):
+        self.task = dict()
+
+    def __str__(self):
+        display = []
+        if self.task:
+            for i_priority in sorted(self.task.keys()):
+                display.append("{prior} {task}\n".format(
+                    prior=str(i_priority),
+                    task=self.task[i_priority]
+                )
+            )
+        return "".join(display)
+        
+    def new_task(self, task, priority):
+        if priority not in self.task:
+            self.task[priority] = Stack()
+        self.task[priority].push(task)
+
+
+
+
+manager = TaskManager()
+manager.new_task("сделать уборку", 4)
+manager.new_task("помыть посуду", 4)
+manager.new_task("отдохнуть", 1)
+manager.new_task("поесть", 2)
+manager.new_task("сдать ДЗ", 2)
+print(manager)
