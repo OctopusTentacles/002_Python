a1264f7a14b518aa0d85206906c2c9bb718be020
Octopus <klinov.artem@gmail.com>
2023-10-22 21:29:27 +0500

    13_2_1_com4 for one more time

--- Part2_Modul13/13_2_Iterators1.py
+++ Part2_Modul13/13_2_Iterators1.py
@@ -7,14 +7,3 @@
 
 import random
 
-amt = int(input("Введите количество чисел: "))
-
-my_list = [random.randint(0, 10) for _ in range(amt)]
-buffer_iter = iter(my_list)
-
-while True:
-    try:
-        print(next(buffer_iter))
-    except StopIteration:
-        print("конец итерации")
-        break
