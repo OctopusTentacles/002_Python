2df819c80e32aaba2047b14c4c9249bf119a8d5b
Octopus <klinov.artem@gmail.com>
2023-10-27 21:34:40 +0600

    Recursive Generators / slices

--- TRAINING/Iter Next Gen/task_9.py
+++ TRAINING/Iter Next Gen/task_9.py
@@ -2,7 +2,11 @@
 
 def permutations(items):
     if len(items) == 0: yield []
+    else:
+        for i in range(len(items)):
+            for j in permutations(items[:i] + items[i+1:]):
+                yield [items[i]] + j
 
 
-for i in permutations(["r", "e, "d]):
+for i in permutations(["r", "e", "d"]):
     print("".join(i))
