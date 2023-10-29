67fd7b216c47af3ac5fb00334725d5de2d59bbae
Octopus <klinov.artem@gmail.com>
2023-10-29 13:17:38 +0600

    13_5_1_com3 fibonacci sequence

--- Part2_Modul13/13_5_generators1.py
+++ Part2_Modul13/13_5_generators1.py
@@ -8,9 +8,16 @@
 
 
 def fibonacci(numbers):
+    """fibonacci sequence"""
     cur_num = 0
     next_num = 1
-    for _ in numbers:
+    for _ in range(numbers):
         yield cur_num
-        cur_num = next_num
-        next_num = cur_num + next_num
+        cur_num, next_num = next_num, cur_num + next_num
+        if cur_num > 10 ** 6:
+            return
+
+
+fib_seq = fibonacci(100)
+for seq in fib_seq:
+    print(seq, end=" ")
