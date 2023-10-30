e752499bbd533bdb8c9b936c6557183612809dc4
Octopus <klinov.artem@gmail.com>
2023-10-30 20:08:52 +0600

    13_5_2_com3 Big file /

--- Part2_Modul13/13_5_generators2.py
+++ Part2_Modul13/13_5_generators2.py
@@ -8,5 +8,38 @@
 # чисел в файле. Для считывания файла реализуйте специальный генератор.
 
 
+def numbers_from_text(text):
+    return [int(number) for number in text.rstrip().split()]
+
+
+def file_parser(path_to_file):
+    with open(path_to_file) as file:
+        for line in file:
+            clear_line_sum = sum(numbers_from_text(line))
+            # https://docs-python.ru/tutorial/vstroennye-funktsii-interpretatora-python/funktsija-map/
+            yield clear_line_sum
+
+
+all_sum = 0
+for number in file_parser("numbers.txt"):
+    all_sum += number
+
+print("Вариант №1", all_sum)
+
+
+# Ещё один вариант решения с функцией map()
+def file_parser(path_to_file):
+    with open(path_to_file) as file:
+        for line in file:
+            clear_line_sum = sum(map(int, line.rstrip().split()))
+            # https://docs-python.ru/tutorial/vstroennye-funktsii-interpretatora-python/funktsija-map/
+            yield clear_line_sum
+
+
+all_sum = 0
+for number in file_parser("numbers.txt"):
+    all_sum += number
+
+print("Вариант №2", all_sum)
 
 
