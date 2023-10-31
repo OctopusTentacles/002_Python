bd963efbd2fdb13ba4adb699670c29459ca08998
Octopus <klinov.artem@gmail.com>
2023-10-31 17:34:13 +0600

    13_6_com2 Type Annotations /

--- Part2_Modul13/13_6_Type Annotations.py
+++ Part2_Modul13/13_6_Type Annotations.py
@@ -1,6 +1,22 @@
 # Type Annotations
 
+
+from 
+
+
 class Person:
 
-    def __init__(self, name: str, age: int, friends: list):
-        
+    def __init__(self, name: str, age: int, friends: list) -> None:
+        self.__name = name
+        self.__age = age
+        self.__friends = friends
+
+    def __str__(self) -> str:
+        pass
+
+
+def fibonacci(number: int) -> Iterable[int]:
+    a, b = 0, 1
+    for _ in range(number):
+        yield a
+        a, b = b, a + b
