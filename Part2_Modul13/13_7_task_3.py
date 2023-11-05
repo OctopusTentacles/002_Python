37344aba4cc28c8c517bf43e43b7e481862bc3b8
Octopus <klinov.artem@gmail.com>
2023-11-05 14:50:20 +0600

    13_7_3_com11 count code lines / stopiter

--- Part2_Modul13/13_7_task_3.py
+++ Part2_Modul13/13_7_task_3.py
@@ -17,32 +17,40 @@ def py_files(folder_name: str, ending='.py') -> str:
 
         ending - расширение файлов.
     """
-    
+    # try:
+
     for dirpath, dirnames, filenames in os.walk('/'):
-        for dirname in dirnames:
-            if folder_name == dirname:
-                my_dir = os.path.join(dirpath, dirname)
-                yield "Каталог:", my_dir
+            for dirname in dirnames:
+                if folder_name == dirname:
+                    my_dir = os.path.join(dirpath, dirname)
+                    yield "Каталог:", my_dir
 
-                for py_file in os.listdir(my_dir):
-                    if py_file.endswith(ending):
-                        yield "\n\tфайл: ", py_file
+                    for py_file in os.listdir(my_dir):
+                        if py_file.endswith(ending):
+                            yield "\n\tфайл: ", py_file
 
-                        amt_lines = 0
-                        with open(os.path.join(my_dir, py_file), 'r', encoding='utf8') as file:
-                            for line in file:
-                                clear_line = line.lstrip()
-                                if not clear_line:
-                                    continue
-                                elif clear_line.startswith("#") or clear_line.startswith('"') or\
-                                    clear_line.startswith('"'):
-                                    continue
-                                else:
-                                    amt_lines += 1
-                            yield "\tстрок кода: ", amt_lines
+                            amt_lines = 0
+                            with open(os.path.join(my_dir, py_file), 'r', encoding='utf8') as file:
+                                for line in file:
+                                    clear_line = line.lstrip()
+                                    if not clear_line:
+                                        continue
+                                    elif clear_line.startswith("#") or clear_line.startswith('"') or\
+                                        clear_line.startswith('"'):
+                                        continue
+                                    else:
+                                        amt_lines += 1
+                                yield "\tстрок кода: ", amt_lines
+                    
+                        else: raise StopIteration("файлы не найдены")
+                else: raise StopIteration("каталог не найден!")
+    # except StopIteration:
                 return
 
 
 catalog = py_files(input("Имя каталога: "))
 for py_file in catalog:
     print(*py_file)
+
+
+# 12_7_task_4
