36702d8da22dab009e157062ff3e355475a05b60
Octopus <klinov.artem@gmail.com>
2023-11-07 00:14:10 +0600

    13_7_4_com7 LinkedList / get

--- Part2_Modul13/13_7_task_4.py
+++ Part2_Modul13/13_7_task_4.py
@@ -58,20 +58,15 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
-    def __iter__(self):
-        while self.head is not None:
-            yield self.head.value
-            self.head = self.head.next_node
-
     def __str__(self):
-        return f"[{' '.join(str(i) for i in self)}]"
-
-    def get(self, index):
+        list_link = []
         node = self.head
-        for _ in range(index):
+        while node.next_node:
+            list_link.append(node.value)
             node = node.next_node
-        return node.value
-
+        list_link.append(node.value)
+        return str(list_link)
+            
     def append(self, value):
         if self.head is None:
             self.head = Node(value)
@@ -80,6 +75,18 @@ class LinkedList:
             while node.next_node:
                 node = node.next_node
             node.next_node = Node(value)
+    
+    def get(self, index):
+        count = 0
+        node = self.head
+
+        while count < index:
+
+            node = node.next_node
+            count += 1
+        return node.value
+
+
 
 
 my_list = LinkedList()
