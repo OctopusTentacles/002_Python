a87b06a74bcaff7a520e52fe52cc919785268e52
Octopus <klinov.artem@gmail.com>
2023-11-09 16:07:05 +0600

    14_1_com2 from typing import Any, Optional

--- Part2_Modul14/14_1_home_task.py
+++ Part2_Modul14/14_1_home_task.py
@@ -6,3 +6,15 @@
 
 # класс Односвязный список:
 #    - указатель на первый узел
+
+
+from typing import Any, Optional
+
+
+class Node:
+    def __init__(self, value: Optional[Any] = None, next: Optional[Node] = None) -> None:
+        self.value = value
+        self.next = next
+
+    def __str__(self) -> str:
+        return "Node [{value}]".format(value=str(self.value))
