550a7b58b366e991addf61168b27cbac862ea00f
Octopus <klinov.artem@gmail.com>
2023-11-13 15:47:02 +0600

    14_3_1_com4 wrapped_func

--- Part2_Modul14/14_3_Decorators1.py
+++ Part2_Modul14/14_3_Decorators1.py
@@ -27,7 +27,8 @@
 def do_twice(func):
     def wrapped_func(*args, **kwargs):
         func(*args, **kwargs)
-
+        return func(*args, **kwargs)
+    return wrapped_func
 
 @do_twice
 def greeting(name: str) -> None:
