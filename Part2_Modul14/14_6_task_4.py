b7aab6a58bfed860333c3566d102c8f062a9d7c9
Octopus <klinov.artem@gmail.com>
2023-11-18 19:13:28 +0600

    14_6_4_com2 draft

--- Part2_Modul14/14_6_task_4.py
+++ Part2_Modul14/14_6_task_4.py
@@ -10,15 +10,21 @@
 
 import random
 import functools
-from typing import Callable
+from typing import Callable, Any
 
 
-def counter(func: Callable) -> Callable:
+def counter(self, func: Callable) -> Callable:
     """ """
-    pass
+
+    self.count = 0
+    def wrapper(*args, **kwargs) -> Any:
+        func(*args, **kwargs)
+        self.count += 1
+        print(count)
+    return wrapper
 
 
 def creat_list():
     """ Создает список и возвращает определенный элемент из списка. """
     my_list = [i for i in range(random.randint(0, 4))]
-    return my_list[3]
+    return my_list
