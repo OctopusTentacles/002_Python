f072be4958219a631d01795934231e2332e2f58c
Octopus <klinov.artem@gmail.com>
2023-11-20 17:22:49 +0500

    15_1_com2 @counter

--- Part2_Modul15/15_1_home_task.py
+++ Part2_Modul15/15_1_home_task.py
@@ -15,7 +15,19 @@ def counter(func: Callable) -> Callable:
     def wrapper(*args, **kwargs):
         wrapper.count += 1
         res = func(*args, **kwargs)
+        print("Функция {func} была вызвана {count} раз.".format(
+            func=func.__name__, count=wrapper.count))
 
         return res
     wrapper.count = 0
     return wrapper
+
+
+@counter
+def test():
+    print("test")
+
+
+test()
+test()
+test()
