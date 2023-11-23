9297ae10905579544c13c9f87ad4c0af8835ff61
Octopus Tentacles <klinov.artem@gmail.com>
2023-11-23 15:00:18 +0500

    15_4_2_com1 class Figure

---
+++ Part2_Modul15/15_4_MRO2.py
@@ -0,0 +1,33 @@
+# Задача 2. Фигуры
+# При моделировании компьютерных объектов используются два типа фигур: 
+# прямоугольники и квадраты. Каждая из них имеет координаты XY, длину и ширину. 
+# Также каждая фигура может менять координаты (двигаться) и менять размер. 
+
+# Реализуйте такие классы. Учтите, что с точки зрения интерфейса прямоугольник и 
+# квадрат — это разные фигуры и работают они по-разному. В частности, по разному 
+# работает метод изменения размера фигуры, так как у квадрата все стороны равны.
+
+from abc import ABC, abstractmethod
+
+class Figure:
+    """
+        Абстрактный класс Фигура
+        Args и Attrs:
+            x(int): координата X
+            y(int): координата Y
+            lenght(int): длина фигуры
+            width(int): ширина фигуры
+    """
+    def __init__(self, x: int, y: int, lenght: int, width: int) -> None:
+        self.x = x
+        self.y = y
+        self.lenght = lenght
+        self.width = width
+        
+    def move(self, x: int, y: int) -> None:
+        self.x = x
+        self.y = y
+
+    def resize(self, lenght: int, width: int) -> None:
+        self.lenght = lenght
+        self.width = width
