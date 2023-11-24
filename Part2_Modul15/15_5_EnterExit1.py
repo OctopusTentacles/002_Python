70ae5c4fd5e73eadbab545c33df161184159532d
Octopus Tentacles <klinov.artem@gmail.com>
2023-11-24 10:26:52 +0500

    15_5_1_com2 draft

--- Part2_Modul15/15_5_EnterExit1.py
+++ Part2_Modul15/15_5_EnterExit1.py
@@ -10,12 +10,16 @@
 
 
 class File:
-    try:
 
-    except
-
-    finally
+    def __init__(self) -> None:
+        pass
         
+    def __enter__(self):
+        pass
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
 
 with File(‘example.txt’, ‘w’) as file:
     file.write(‘Всем привет!’)
