ccce821e8a136fa5dd690ded0d12b376f3902b2d
Octopus <klinov.artem@gmail.com>
2023-11-25 23:17:36 +0500

    15_8_com5 pygtrie

--- Part2_Modul15/15_8_data_structures.py
+++ Part2_Modul15/15_8_data_structures.py
@@ -226,4 +226,46 @@ hash_table.delete("banana")
 # Проверяем, что элемент удалён
 print(hash_table.search("banana")) # Вывод: None
 
+# =======================================================================================
+
 # Библиотека pygtrie
+# Установка библиотеки pygtrie
+
+# Убедитесь, что на вашем компьютере установлен Python.
+# Откройте командную строку или терминал.
+# Установите библиотеку pygtrie с помощью команды pip:
+# pip install pygtrie==2.5.0
+
+#from pygtrie import Trie
+
+# Создание экземпляра дерева префиксов
+trie = Trie()
+
+# Вставка элементов в дерево
+trie["apple"] = 1
+trie["banana"] = 2
+trie["orange"] = 3
+
+# Проверка наличия элементов
+print("apple" in trie)  # Вывод: True
+print("pear" in trie)  # Вывод: False
+
+# Получение значения элемента
+print(trie["banana"])  # Вывод: 2
+
+# Удаление элемента
+del trie["banana"]
+print("banana" in trie)  # Вывод: False
+
+# Поиск всех элементов с заданным префиксом
+prefix_items = trie.items("a")
+print(list(prefix_items)) # Вывод: [(('a', 'p', 'p', 'l', 'e'), 1)]
+
+# Автодополнение
+prefix = "app"
+matching_suggestions = list(trie.iterkeys(prefix))
+print(matching_suggestions)  # Вывод: [('a', 'p', 'p', 'l', 'e')]
+
+# =======================================================================================
+
+# Библиотека marisa-trie
