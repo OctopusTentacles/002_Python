9dfb0f19f9d49e3ba203b3e273b942ce5247fe87
Octopus Tentacles <klinov.artem@gmail.com>
2023-11-26 10:48:31 +0500

    15_9_1com4 work with files

--- Part2_Modul15/15_9_task_1.py
+++ Part2_Modul15/15_9_task_1.py
@@ -8,25 +8,36 @@
 import os
 
 class File:
-    def __init__(self, file_name:str, mode:str = "a") -> None:
+    def __init__(self, file_name: str, mode: str = "r") -> None:
+        """ контекст-менеджер File - при попытке открыть несуществующий 
+            файл - автоматически создает  и открывает этот файл в режиме записи
+        """
         self.file_name = file_name
         self.mode = mode
         self.file = None
 
     def __enter__(self):
+        """ метод для входа в блок контекст-менеджера
+        """
         cur_dir = os.path.join(os.path.dirname(__file__), self.file_name)
 
         try:
-            self.file = open(cur_dir, self.mode)
+            # открываем файл для чтения
+            self.file = open(cur_dir, self.mode, encoding="utf8")
+            # print("файл открыт для чтения!")
 
         except FileNotFoundError: 
+            # но если файл не существует - создаем и открываем для записи
             self.file = open(cur_dir, "w", encoding="utf8")
+            # print("файл создан и открыт для записи!")
 
         return self.file
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.file.close()
-        return True # для исключения любых ошибок
+    def __exit__(self, exc_type, exc_value, traceback) -> bool:
+        """ метод для выхода из блока контекст-менеджера
+        """
+        # подавление всех исключений
+        return True 
 
 
 with File("example.txt") as file:
