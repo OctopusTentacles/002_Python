1366148ddda37228ce26dd0f5559d6d1f2d48ca5
Octopus Tentacles <klinov.artem@gmail.com>
2023-11-26 10:19:35 +0500

    15_9_1com3 cur_dir try open

--- Part2_Modul15/15_9_task_1.py
+++ Part2_Modul15/15_9_task_1.py
@@ -5,20 +5,29 @@
 # создавать и открывать этот файл в режиме записи; 
 # на выходе из менеджера должны подавляться все исключения, связанные с файлами.
 
+import os
 
 class File:
-    def __init__(self, file_name:str, mode:str = "w") -> None:
+    def __init__(self, file_name:str, mode:str = "a") -> None:
         self.file_name = file_name
         self.mode = mode
         self.file = None
 
     def __enter__(self):
-        self.file = open(self.file_name, self.mode, encoding="utf8")
-        pass
+        cur_dir = os.path.join(os.path.dirname(__file__), self.file_name)
+
+        try:
+            self.file = open(cur_dir, self.mode)
+
+        except FileNotFoundError: 
+            self.file = open(cur_dir, "w", encoding="utf8")
+
+        return self.file
 
     def __exit__(self, exc_type, exc_value, traceback):
-        pass
+        self.file.close()
+        return True # для исключения любых ошибок
 
 
-with File("example.txt", "r") as file:
-    file.write()
+with File("example.txt") as file:
+    file.write("Всем привет!")
