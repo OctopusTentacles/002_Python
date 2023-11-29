2a67baa5f8da146551787fc0f419e08956bfae95
Octopus Tentacles <klinov.artem@gmail.com>
2023-11-29 13:52:00 +0500

    16_2_1_com2 draft

--- Part2_Modul16/16_2_context_manager1.py
+++ Part2_Modul16/16_2_context_manager1.py
@@ -3,3 +3,22 @@
 # функция должна работать с оператором with и замерять время работы 
 # вложенного кода.
 
+
+from contextlib import contextmanager
+from collections.abc import Iterator
+
+@contextmanager
+def next_num(num: int) -> Iterator[int]:
+    print("Входим в функцию")
+    try:
+        yield num + 1
+    except ZeroDivisionError as exc:
+        print("Обнаружена ошибка:", exc)
+    finally:
+        print("Код выполняется")
+    print("Выход из функции")
+
+
+with next_num(-1) as next:
+    print("Следующее число = {}".format(next))
+    print(10 / next)
