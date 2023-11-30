28098ca454aa17a109cddd2b97cbbb25c106e991
Octopus Tentacles <klinov.artem@gmail.com>
2023-11-30 16:16:02 +0500

    16_3_1_com2 def repeat(n)

--- Part2_Modul16/16_3_@args1.py
+++ Part2_Modul16/16_3_@args1.py
@@ -5,14 +5,18 @@
 # функцию уже n раз. 
 
 
+def repeat(qnt: int):
+    def do_twise(func):
+        def wrapper(*args, **kwargs):
+            for _ in range(qnt):
+                func(*args, **kwargs)
+            return 
+        return wrapper
+    return do_twise
 
-def do_twise(func):
-    def wrapper(*args, **kwargs):
-        func(*args, **kwargs)
-        return 
-    return wrapper
 
-
-
+@repeat(5)
 def greeting(name: str) -> None:
-    print('Привет, Tome!')
+    print('Привет, {name}!'.format(name = name))
+
+greeting("Tom")
