d1430cb93cc5f22656cf11b3c38785ef37ac1106
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-01 16:01:00 +0500

    16_4_1_com6

--- Part2_Modul16/16_4_@class1.py
+++ Part2_Modul16/16_4_@class1.py
@@ -73,3 +73,36 @@ my_funcs_1 = Functions(max_num=1000)
 
 my_funcs_1.squares_sum()
 my_funcs_1.cubes_sum(number=2000)
+
+# =============================================================================
+
+import datetime
+
+
+def decorator(cls):
+    def wrapper(*args, **kwargs):
+        instance = cls(*args, **kwargs)
+
+        print("Время создания -", datetime.datetime.now())
+        print("Методы:", end=" ")
+        for i_method in dir(cls):
+            if i_method.startswith('__'):
+                continue
+            print(i_method, end=' ')
+        print()
+        return instance
+    return wrapper
+
+
+@decorator
+class Example:
+
+    def hello(self):
+        print("hello")
+
+    def hello_2(self):
+        print("hello")
+
+
+Example()
+Example()
