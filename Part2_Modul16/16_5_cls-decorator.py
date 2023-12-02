7f1634bc0e861de075d5795cd3d141a68fb2ef9e
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-02 13:51:22 +0500

    16_5_com2 CountCalls / functools

--- Part2_Modul16/16_5_cls-decorator.py
+++ Part2_Modul16/16_5_cls-decorator.py
@@ -3,7 +3,7 @@
 # Написание класса-декоратора CountCalls
 # Создание декоратора с помощью метода call
 
-from typing import Callable
+from typing import Any, Callable
 import functools
 
 class CountCalls:
@@ -12,7 +12,15 @@ class CountCalls:
         self.func = func
         self.num_calls = 0
 
+    def __call__(self, *args: Any, **kwds: Any) -> Callable:
+        # x() = x.__call__
+        self.num_calls += 1
+        print("Вызов номер {num} функции {func}".format(
+            num=self.num_calls, func=self.func.__name__
+        ))
+        return self.func(*args, **kwds)
 
+@CountCalls
 def say_hello():
     print("Hello!")
 
@@ -20,3 +28,5 @@ def say_hello():
 say_hello()
 say_hello()
 say_hello()
+
+print(say_hello.num_calls)
