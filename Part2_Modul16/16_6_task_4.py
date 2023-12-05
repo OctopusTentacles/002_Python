38503a4dfd918f7de15d9078b97699d0c3aeb0ff
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-05 16:19:21 +0500

    16_6_4_com2 draft

--- Part2_Modul16/16_6_task_4.py
+++ Part2_Modul16/16_6_task_4.py
@@ -12,23 +12,22 @@ import functools
 from typing import Callable, Optional
 
 
-def decorator_with_args_for_any_decorator(func: Callable, *args, **kwargs):
-    @functools.wraps(func)
+def decorator_with_args_for_any_decorator(decorator: Callable):
+    @functools.wraps(decorator)
     def wrapper(*args, **kwargs):
 
         print("Переданные арги и кварги в декоратор:", *args, **kwargs)
 
-        return func(*args, **kwargs)
+        return decorator
     return wrapper
 
 
 @decorator_with_args_for_any_decorator
-def decorated_decorator(func: Callable, *args, **kwargs):
-    
+def decorated_decorator(func: Callable):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
 
-        print("Переданные арги и кварги в декоратор:", *args, **kwargs)
+        
 
         return func(*args, **kwargs)
     return wrapper
