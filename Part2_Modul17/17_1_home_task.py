1a0ddebf14b990e2b8817a859143820a424ea1a2
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-07 10:33:29 +0500

    17_1_com4 singleton

--- Part2_Modul17/17_1_home_task.py
+++ Part2_Modul17/17_1_home_task.py
@@ -41,6 +41,20 @@ decorated_function("Юзер", 101)
 # инстанс, а все остальные попытки создания будут возвращать первый экземпляр.
 
 
+def singleton(cls):
+    """ Декоратор класса. Превращает класс в синглтон
+        (может иметь только один инстанс)
+    """
+    @functools.wraps(cls)
+    def wrapper_singleton(*args, **kwargs):
+        if not wrapper_singleton.instance:
+            wrapper_singleton.instance = cls(*args, **kwargs)
+        return wrapper_singleton.instance
+
+    wrapper_singleton.instance = None #кэш
+    return wrapper_singleton
+
+
 
 @singleton
 class Example:
