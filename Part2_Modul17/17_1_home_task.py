d5cb119748b6854a14cbcc4c4c82de1f8e062b85
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-07 10:01:20 +0500

    17_1_com2

--- Part2_Modul17/17_1_home_task.py
+++ Part2_Modul17/17_1_home_task.py
@@ -7,12 +7,22 @@ import functools
 from typing import Callable, Any
 
 
+def decorator_with_args_for_any_decorator(decorator_to_enhance: Callable) -> Callable:
+    """ Декоратор. Дает возможность другому декоратору принимать аргументы """
+    def decorator_maker(*args, **kwargs) -> Callable:
+        def decorator_wrapper(func: Callable) -> Callable:
+            return decorator_to_enhance(func, *args, **kwargs)
+        return decorator_wrapper
+    return decorator_maker
+
+@decorator_with_args_for_any_decorator
 def decorated_decorator(func: Callable, *dec_args, **dec_kwargs) -> Callable:
     """ Декоратор. Шаблон"""
     @functools.wraps(func)
     def wrapper(*f_args, **f_kwargs) -> Callable:
         print("Переданные арги и кварги в декоратор:", dec_args, dec_kwargs)
-
+        return func(*f_args, **f_kwargs)
+    return wrapper
 
 @decorated_decorator(100, 'рублей', 200, 'друзей')
 def decorated_function(text: str, num: int) -> None:
