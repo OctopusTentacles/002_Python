759491d5b8fe2c198882ba5e2d731792b903bb5d
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-09 15:56:32 +0500

    17_4_2_com2 draft

--- Part2_Modul17/17_5_map_filter1.py
+++ Part2_Modul17/17_5_map_filter1.py
@@ -11,12 +11,30 @@
 from typing import List
 
 
+num = input("Введите числа: ")
+list_num = [sym for sym in num.split(' ')]
+print(list_num)
 
 
-
+print('='*50)
 # =============================================================================
+# =============================================================================
+print()
 nums_1: List[int] = [3, 1, 4, 1, 5, 9, 2, 6]
 nums_2: List[int] = [2, 7, 1, 8, 2, 8, 1, 8]
 
 result: List[int] = list(map(lambda x, y: x + y, nums_1, nums_2))
 print(result)
+
+result_even: List[int] = list(filter(lambda x: x % 2 == 0, result))
+print(result_even)
+# =============================================================================
+animals = ['cat', 'dog', 'cow'] # ['Cat', 'Dog', 'Cow']
+# with map:
+new_animals = list(map(lambda elem: elem.capitalize(), animals))
+# with list comprehension:
+new_animals = [elem.capitalize() for elem in animals]
+# =============================================================================
+
+result = map(lambda num: num * 3, filter(lambda num: num % 2, nums_1))
+print(list(result))
