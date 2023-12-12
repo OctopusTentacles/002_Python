8fc120f7ee422a9926c7479465c49886258990a5
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-12 14:29:50 +0500

    17_8_4_com3 letter.lower().count(sym)

--- Part2_Modul17/17_8_task_4.py
+++ Part2_Modul17/17_8_task_4.py
@@ -14,18 +14,20 @@
 # (например строка «аа» будет содержать 0 уникальных букв).
 
 
-from collections import Counter
+# from collections import Counter
+
 
 def count_unique_characters(letter: str) -> int:
+    """ Функция принимает строку и возвращает количество уникальных символов.
+        param letter: входная строка;
+        return int: кол-во уникальных символов в строке.
     """
-    """
-    low_letter = Counter(letter.lower())
-    print(low_letter)
-    uniq_char = sum(filter(lambda sym: sym == 1, low_letter.values()))
-    # uniq_char = list(filter(lambda sym: letter.lower().count(sym) == 1, letter.lower()))
-    print(uniq_char)
+    # low_letter = Counter(letter.lower())
+    # uniq_char = sum(filter(lambda value: value == 1, low_letter.values()))
 
-    return (uniq_char)
+    uniq_char = list(filter(lambda sym: letter.lower().count(sym) == 1, letter.lower()))
+
+    return len(uniq_char)
 
 
 message = "Today is a beautiful day! The sun is shining and the birds are singing."
