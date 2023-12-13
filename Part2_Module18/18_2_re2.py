2f8f6b21fae68d6a820fb770cc69d0572652023c
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-13 13:35:12 +0500

    18_2_2_com2 re.findall

--- Part2_Module18/18_2_re2.py
+++ Part2_Module18/18_2_re2.py
@@ -28,6 +28,6 @@ import re
 
 
 text = "How much \wwood+?, would a \wwood+?chuck \dwwood+, chuck if a \wwood+?,chuck could chuck \wwood?"
+result = re.findall(r"\\wwood\+\?,", text)
 
-
-print("",)
+print("Список всех упоминаний шаблона:", result)
