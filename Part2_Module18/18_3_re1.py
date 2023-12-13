9f22bacefe8eff51553f7129c087577f8df6ee4c
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-13 16:08:29 +0500

    18_3_1_com2 reg expressions

--- Part2_Module18/18_3_re1.py
+++ Part2_Module18/18_3_re1.py
@@ -19,6 +19,17 @@
 
 import re
 
+text = "Even if they are djinns, I will get djinns that can outdjinn them."
+
+
+result_1 = re.findall(r"\b[aeiouAEIOU]\w*", text)
+# result_2 = re.findall(r"\b[^ ,.aAeEyYuUiIoO]\w*", text)
+result_2 = re.findall(r"\b[^aAeEyYuUiIoO\W]\w*", text)
+
+
+print("Слова на гласную:", result_1)
+print("Слова на любой символ, кроме гласной:", result_2)
+
 
 # Ожидаемый результат:
 # Слова на гласную: ['Even', 'if', 'are', 'I', 'outdjinn']
