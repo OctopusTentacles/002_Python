a168f84a2a9701f8144bc356747248fd61d73d88
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-14 15:18:58 +0500

    18_4_1_com5

--- Part2_Module18/18_4_JSON1.py
+++ Part2_Module18/18_4_JSON1.py
@@ -44,3 +44,15 @@ with open('my_test.json', 'r') as file:
     data = json.load(file)
 
 print(data)
+
+# =======================================================================================
+result = requests.get("https://swapi.dev/api/people/3/")
+if result.status_code == 200:
+    json_dict = json.loads(result.text)
+    json_dict['name'] = input("Введите имя: ")
+    with open("swap.json", "w") as file:
+        json_text = json.dump(json_dict, file, indent=4)
+
+    # Доп:
+    result_homeworld = requests.get(json_dict['homeworld'])
+    print(result_homeworld.text)
