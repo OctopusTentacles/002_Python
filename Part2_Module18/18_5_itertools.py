d03b9d02251759351b098b560b2d3479f82f7fb4
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-15 08:48:45 +0500

    18_5_com2 itertools.cycle

--- Part2_Module18/18_5_itertools.py
+++ Part2_Module18/18_5_itertools.py
@@ -12,3 +12,11 @@ print('=' * 50)
 
 for item in itertools.combinations(colors, 2):
     print(item)
+
+print('=' * 50)
+my_cycle = itertools.cycle(['one', 'two', 'three'])
+print(next(my_cycle))
+print(next(my_cycle))
+print(next(my_cycle))
+print(next(my_cycle))
+print(next(my_cycle))
