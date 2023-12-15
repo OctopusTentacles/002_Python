5cfd2549c980000244cf717751a7307bbf778c54
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-15 08:19:07 +0500

    18_4_2_com2 json.dump

--- Part2_Module18/18_4_JSON2.py
+++ Part2_Module18/18_4_JSON2.py
@@ -13,6 +13,15 @@ import requests
 import json
 
 
-my_req = requests.get('https://swapi.dev/api/')
-print(my_req)
+my_req = requests.get('https://swapi.dev/api/films/1/')
+# print(my_req)
 # Получаем <Response [200]> - запрос произведен успешно
+
+# print(my_req.text)
+if my_req.status_code == 200:
+    json_dict = json.loads(my_req.text)     # десериализация JSON
+
+    with open('my_test.json', 'w') as file:
+        json.dump(json_dict, file, indent=4)    # сериализация JSON
+
+    print(json_dict['opening_crawl'])
