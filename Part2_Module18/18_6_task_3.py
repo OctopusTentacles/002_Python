0fe9359dbe74458aa30517a0ab1da7b3c50cedc3
Octopus <klinov.artem@gmail.com>
2023-12-16 23:34:59 +0500

    18_6_3_com11 requests / docs

--- Part2_Module18/18_6_task_3.py
+++ Part2_Module18/18_6_task_3.py
@@ -23,19 +23,25 @@
 import os
 import json
 import requests
+from typing import Dict, Any
 
-
+# чтобы созданный файл был в одно каталоге:
 cur_dir = os.path.dirname(__file__)
 
 
-def api_info(url, name):
+def api_info(url: str, name: str) -> Dict[str, Any]:
+    """ функция для поиска информации по ресурсу API.
+        param url: ссылка на ресурс API.
+        param name: имя того что нас интересует.
+    """
 
+    # запрос:
     response = requests.get(url)
-    # print(my_request)
-    # results - список словарей всех кораблей:
     if response.status_code == 200:
+        # десериализация JSON:
         ship_data = json.loads(response.text)
 
+        # все данные лежат в ключе results:
         for result in ship_data['results']:
             if result["name"] == name:
                 starship_info = {
@@ -45,10 +51,13 @@ def api_info(url, name):
                     "список пилотов": []                    
                 }
 
+                # список пилотов содержит ссылки:
                 for url_pilot in result["pilots"]:
                     request_pilot = requests.get(url_pilot)
                     if request_pilot.status_code == 200:
+                        # десериализация JSON:
                         pilot_data = json.loads(request_pilot.text)
+                        # словарь на каждого пилота:
                         pilot_info = {
                             "имя": pilot_data["name"],
                             "рост": pilot_data["height"],
@@ -56,23 +65,27 @@ def api_info(url, name):
                             "родная планета": "",
                             "ссылка на информацию о родной планете": pilot_data["homeworld"]
                         }
+                    # родная планета - ссылка, а нам нужно имя -> str:
                     request_planet = requests.get(pilot_data["homeworld"])
                     if request_planet.status_code == 200:
+                        # десериализация JSON:
                         planet_data = json.loads(request_planet.text)
                         pilot_info["родная планета"] = planet_data["name"]
+
+                        # добавляем в список пилотов словарь на каждого пилота:
                         starship_info["список пилотов"].append(pilot_info)
         return starship_info
     
 
+# MAIN===================================================================================
 my_url = "https://swapi.dev/api/starships/"
 search_name = "Millennium Falcon"
 
 starship = api_info(my_url, search_name)
 
+# сериализация JSON и запись в файл:
 with open(os.path.join(cur_dir, "info.json"), "w", encoding='utf-8') as file:
-    json.dump(starship, file, ensure_ascii=False, indent=4)
+    json.dump(starship, file, indent=4, ensure_ascii=False)
 
-with open(os.path.join(cur_dir, "info.json"), "r", encoding='utf-8') as file:
-    data = json.load(file)
-
-print(data)
+    # вывод в консоль в JSON формате:
+    print(json.dumps(starship, indent=4, ensure_ascii=False))
