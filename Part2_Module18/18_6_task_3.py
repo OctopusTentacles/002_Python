90a22e4a5e03446c7960980ace12c9f003f5a8b9
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-16 08:47:50 +0500

    18_6_3_com3 found my ship

--- Part2_Module18/18_6_task_3.py
+++ Part2_Module18/18_6_task_3.py
@@ -30,6 +30,15 @@ cur_dir = os.path.dirname(__file__)
 
 my_request = requests.get("https://swapi.dev/api/starships/")
 # print(my_request)
+# results - список словарей всех кораблей:
 if my_request.status_code == 200:
-    data = json.loads(my_request.text)
-    print(data['name'])
+
+    json_ships = json.loads(my_request.text)
+    # print(json_ships)
+    all_starship = []
+    for result in json_ships['results']:
+        if result["name"] == "Millennium Falcon":
+            url_ship = result["url"]
+
+    my_request = requests.get(url_ship).json()
+    print(my_request)
