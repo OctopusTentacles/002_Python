81fd4a04d1d607b8325779608ed07825de1dfda0
Octopus Tentacles <klinov.artem@gmail.com>
2023-12-16 14:04:00 +0500

    18_6_3_com5 get pilots

--- Part2_Module18/18_6_task_3.py
+++ Part2_Module18/18_6_task_3.py
@@ -27,33 +27,47 @@ import requests
 
 cur_dir = os.path.dirname(__file__)
 
-url = "https://swapi.dev/api/starships/"
+my_url = "https://swapi.dev/api/starships/"
 search_name = "Millennium Falcon"
 
-my_request = requests.get(url)
-# print(my_request)
-# results - список словарей всех кораблей:
-if my_request.status_code == 200:
 
-    json_ships = json.loads(my_request.text)
-    # print(json_ships)
-    for result in json_ships['results']:
-        if result["name"] == search_name:
+def api_info(url, info, name):
 
-            ship_info = {'name': result["name"], 
-                         "max_atmosphering_speed": result["max_atmosphering_speed"],
-                         "starship_class": result["starship_class"],
-                         "pilots": result["pilots"]
-                         }
-            print(ship_info)
+    my_request = requests.get(url)
+    # print(my_request)
+    # results - список словарей всех кораблей:
+    if my_request.status_code == 200:
 
-            # falcon_ship = requests.get(result["url"])
-            # json_dict = json.loads(falcon_ship.text)
-            # ship_info = json_dict.get("name", )
+        json_data = json.loads(my_request.text)
+        # print(json_ships)
+        for result in json_data['results']:
+            if result["name"] == name:
+                dict_info = {}
+                for key_1 in info:
+                    # if key_1 == "pilots":
+                    #     for key_2 in pilot_info:
+                    #         dict_info[key_1][key_2] = result[key_1](key_2)
+                    dict_info[key_1] = result[key_1]
+                print(dict_info)
 
-            # with open(os.path.join(cur_dir, "falcon.json"), "w") as file:
-            #     json_falcon = json.dump(json_dict, file, indent=4)
 
-            
-            # print(json_falcon)
+            # ship_info = {'name': result["name"], 
+            #              "max_atmosphering_speed": result["max_atmosphering_speed"],
+            #              "starship_class": result["starship_class"],
+            #              "pilots": result["pilots"]
+            #              }
+            # # print(ship_info)
 
+
+            # with open(os.path.join(cur_dir, "info.json"), "w") as file:
+            #     json_falcon = json.dump(ship_info, file, indent=4)
+
+            # with open(os.path.join(cur_dir, "info.json"), "r") as file:
+            #     print(json.load(file))
+
+
+
+ship_info = ["name", "max_atmosphering_speed", "starship_class", "pilots"]
+pilot_info = ["name", "height", "mass", "homeworld", "homeworld"]
+
+starship = api_info(my_url, ship_info, search_name)
