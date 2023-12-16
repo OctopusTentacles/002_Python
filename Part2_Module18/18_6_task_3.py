c08327c48b5672f54ce6bc2fdc41257304c984a3
Octopus <klinov.artem@gmail.com>
2023-12-16 18:34:24 +0500

    18_6_3_com7 pilot_info

--- Part2_Module18/18_6_task_3.py
+++ Part2_Module18/18_6_task_3.py
@@ -27,59 +27,39 @@ import requests
 
 cur_dir = os.path.dirname(__file__)
 
-my_url = "https://swapi.dev/api/starships/"
-search_name = "Millennium Falcon"
 
-ship_info = ["name", "max_atmosphering_speed", "starship_class", "pilots"]
-pilot_info = ["name", "height", "mass", "homeworld", "homeworld"]
+def api_info(url, name):
 
-dict_info = {}
+    response = requests.get(url)
+    # print(my_request)
+    # results - список словарей всех кораблей:
+    if response.status_code == 200:
+        ship_data = json.loads(response.text)
 
-my_request = requests.get(my_url)
-if my_request.status_code == 200:
-    json_data = json.loads(my_request.text)
-
-    for result in json_data["results"]:
-        if result == search_name:
-            
-
-
-    # with open(os.path.join(cur_dir, "info.json"), "w") as file:
-    #     json_falcon = json.dump(ship_info, file, indent=4)
-
-    # with open(os.path.join(cur_dir, "info.json"), "r") as file:
-    #     print(json.load(file))
-
-
-
-# def api_info(url, info, name=None):
-
-#     my_request = requests.get(url)
-#     # print(my_request)
-#     # results - список словарей всех кораблей:
-#     if my_request.status_code == 200:
-
-#         json_data = json.loads(my_request.text)
-#         # print(json_ships)
-#         for key_1 in info:
-
-#         for result in json_data['results']:
-#             if result["name"] == name:
-#                 dict_info = {}
-#                 for key_1 in info:
-#                     if isinstance(result[key_1], list):
-#                         for key_2 in result[key_1]:
-#                             return api_info(key_2, pilot_info)
-#                     dict_info[key_1] = result[key_1]
-#                 print(dict_info)
+        for result in ship_data['results']:
+            if result["name"] == name:
+                starship_info = {
+                    "название": result["name"], 
+                    "максимальная скорость": result["max_atmosphering_speed"],
+                    "класс": result["starship_class"],
+                    "список пилотов": []                    
+                }
+                for url_pilot in result["pilots"]:
+                    request_pilot = requests.get(url_pilot)
+                    if request_pilot.status_code ==200:
+                        pilot_data = json.loads(request_pilot.text)
+                        pilot_info = {
+                            "имя": 
+                            "рост":
+                            "вес":
+                            "родная планета":
+                            "ссылка на информацию о родной планете":
+                        }
 
 
-            # ship_info = {'name': result["name"], 
-            #              "max_atmosphering_speed": result["max_atmosphering_speed"],
-            #              "starship_class": result["starship_class"],
-            #              "pilots": result["pilots"]
-            #              }
-            # # print(ship_info)
+
+
+
 
 
             # with open(os.path.join(cur_dir, "info.json"), "w") as file:
@@ -90,7 +70,7 @@ if my_request.status_code == 200:
 
 
 
-# ship_info = ["name", "max_atmosphering_speed", "starship_class", "pilots"]
-# pilot_info = ["name", "height", "mass", "homeworld", "homeworld"]
+my_url = "https://swapi.dev/api/starships/"
+search_name = "Millennium Falcon"
 
-# starship = api_info(my_url, ship_info, search_name)
+starship = api_info(my_url, search_name)
