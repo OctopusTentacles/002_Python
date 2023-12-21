689c857ee0e897d0aece2fe7fd677a5a8fede40e
Octopus <klinov.artem@gmail.com>
2023-12-21 16:41:53 +0600

    19_2_com6 requests

--- Part2_Module19/19_2_BotApi_2.py
+++ Part2_Module19/19_2_BotApi_2.py
@@ -15,4 +15,57 @@
 # https://yandex.ru/dev/dictionary/
 
 # Делаем пробные запросы
-# 
+# Перейдя по ссылке ниже, вы можете увидеть JSON со списком доступных направлений перевода:
+# https://dictionary.yandex.net/api/v1/dicservice.json/getLangs?key=ВАШ_API_КЛЮЧ
+
+# Посмотрим, что с этим можно сделать в методе lookup. Здесь необходимо вводить 
+# дополнительные параметры: направление перевода и текст. 
+# Также есть необязательные: язык интерфейса и опции поиска.
+# Возьмём слово «задача» и направление ru-en. Вставим их в URL-параметры:
+# https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=ВАШ_API_КЛЮЧ&lang=ru-en&text=задача
+# В ответ получим различные переводы и синонимы. Проделайте то же самое у себя в браузере.
+
+# Делаем запросы с помощью requests
+# Сделаем то же самое, только с помощью Python. Для начала установим модуль 
+# requests — он понадобится для отправки запросов:
+
+# pip install requests
+import requests
+from pprint import pprint
+
+API_KEY = 'ВАШ_API_КЛЮЧ'
+BASE_URL = 'https://dictionary.yandex.net/api/v1/dicservice.json'
+
+def get_langs():
+    response = requests.get(f'{BASE_URL}/getLangs', params={
+        'key': API_KEY
+    })
+    return response
+
+def lookup(lang, text, ui='ru'):
+    response = requests.get(f'{BASE_URL}/lookup', params={
+        'key': API_KEY,
+        'lang': lang,
+        'text': text,
+        'ui': ui
+    })
+    return response
+
+langs_response = get_langs()
+if langs_response.status_code != 200:
+    print('Не удалось получить список направлений перевода')
+    exit(1)
+
+langs = langs_response.json()
+print('Выберите одно из доступных направлений перевода')
+print(langs)
+while (lang := input('Введите направление: ')) not in langs:
+    print('Такого направления нет. Попробуйте ещё раз')
+
+text = input('Введите слово или фразу для перевода: ')
+lookup_response = lookup(lang, text)
+if lookup_response.status_code != 200:
+    print('Не удалось выполнить перевод:', lookup_response.text)
+    exit(1)
+
+pprint(lookup_response.json())
