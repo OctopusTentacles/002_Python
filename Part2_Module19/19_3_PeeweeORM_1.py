fc9b3421f6a4c0a4bdd14c0f2c5e9d07b4f82e09
Octopus <klinov.artem@gmail.com>
2023-12-23 18:15:10 +0600

    19_3_com3

--- Part2_Module19/19_3_PeeweeORM_1.py
+++ Part2_Module19/19_3_PeeweeORM_1.py
@@ -68,4 +68,28 @@ class User(Model):
 # создаём таблицу users в базе данных
 db.create_tables([User])
 
-# 
+# Добавим пользователей в базу данных:
+user1 = User(name="Дима", age=25)
+user1.save()
+user2 = User(name="Костя", age=30)
+user2.save()
+
+# Выведем пользователей:
+users = User.select() # получаем список пользователей
+for user in users:
+    print(user.name, user.age)
+# Дима 25
+# Костя 30
+    
+# Для получения объекта из базы используется метод .get с перечислением критериев поиска:
+retrieved_user = User.get(User.name == "Дима")
+
+# Изменить модель так же просто, как и создать её:
+retrieved_user.name = "Дмитрий"
+retrieved_user.save()
+
+# Удалить модель можно с помощью метода .delete_instance:
+user2.delete_instance()
+
+# Мы рассмотрели базовые возможности Peewee. Теперь создадим приближенный 
+# к реальности проект в связке с pyTelegramBotAPI.
