f7d12aca08b4c5b9763e92aed83fdf6fc1eeade0
Octopus <klinov.artem@gmail.com>
2023-12-24 22:26:46 +0600

    Str_9 com1 Palindrom

---
+++ TRAINING/Strings/task_9.py
@@ -0,0 +1,17 @@
+# Напишите функцию is_palindrome(s), которая принимает строку s и возвращает True, 
+# если строка является палиндромом, и False в противном случае. Палиндром - 
+# это строка, которая читается одинаково слева направо и справа налево, 
+# игнорируя пробелы, знаки препинания и регистр.
+
+# Примеры:
+
+
+def is_palindrome(text: str) -> bool:
+
+    pass
+
+
+
+print(is_palindrome("radar"))  # True
+print(is_palindrome("A man a plan a canal Panama"))  # True
+print(is_palindrome("hello"))  # False
