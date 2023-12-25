6cf502a9d21c117de6c142af3e7bdee32af44d46
Octopus <klinov.artem@gmail.com>
2023-12-25 15:17:32 +0600

    Str_9 com5 Palindrom / re / Counter / map

--- TRAINING/Strings/task_9.py
+++ TRAINING/Strings/task_9.py
@@ -3,31 +3,26 @@
 # это строка, которая читается одинаково слева направо и справа налево, 
 # игнорируя пробелы, знаки препинания и регистр.
 
-# Примеры:
-
 
 import re
 from collections import Counter
 
-
 def is_palindrome(text: str) -> bool:
+    if not text:
+        return False
     
-    # Большие буквы преобразуем в маленькие:
-    low_text = text.lower()
-    # убираем пробелы и все спецсимволы:
-    new_text = re.sub(r"[^aA-zZ]", "", low_text)
-
-    # считаем кол-во каждого элемента:
-    char_count = Counter(new_text)
-
-    # считаем кол-во нечетных - их должно быть не больше 1
-    odd_count = sum(map(lambda value: value % 2 !=0, char_count.values()))
-
-    # если нечетных не больше 1 - True:
+    # Преобразование к нижнему регистру и удаление пробелов и спецсимволов:
+    cleaned_text = re.sub(r"[^a-zA-Zа-яА-Я]", "", text.lower())
+    # кол-во каждого символа:
+    char_count = Counter(cleaned_text)
+    # считаем сколько нечетных:
+    odd_count = sum(i % 2 != 0 for i in char_count.values())
+    # если нечетных не больше одного - True:
     return odd_count <= 1
 
 
-
-print(is_palindrome("radar"))  # True
-print(is_palindrome("A man, a plan a canal Panama!"))  # True
-print(is_palindrome("hello"))  # False
+# Тестирование
+print(is_palindrome("A man a plan a canal Panama"))  # True
+print(is_palindrome("Привет, друг! Да, туда где ты!" ))  # True
+print(is_palindrome("Hello, World!"))  # False
+print(is_palindrome(""))  # False
