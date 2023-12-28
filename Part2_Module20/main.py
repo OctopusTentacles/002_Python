820c003f1b587e1183ffebe6277d35a007208d76
Octopus <klinov.artem@gmail.com>
2023-12-28 16:15:42 +0600

    telebot com10 get_data

--- Part2_Module20/main.py
+++ Part2_Module20/main.py
@@ -2,12 +2,25 @@
 # «Привет» (необходимо сообщить проверяющему куратору имя вашего бота для
 # тестирования).
 
-
 import telebot
+import requests
 from config import USERNAME, BOT_TOKEN, API_KEY
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=["get_data"])
+def get_data(message):
+    api_url = "https://"
+    params = {"api_key": API_KEY, "chat_id": message.chat.id}
+    # try:
+        # Отправка GET-запроса к API:
+    response = requests.get(api_url)
+    if response.status_code == 200:
+            data = response.json()
+            bot.send_message(message.chat.id, f"Получены данные: {data}")
+
+
 
 @bot.message_handler(commands=["hello-world"])
 def greet_message(message):
