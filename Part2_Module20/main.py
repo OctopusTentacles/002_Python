2e75db3b24fdda4c05fde99f25bd93c04a205550
Octopus <klinov.artem@gmail.com>
2023-12-29 17:27:19 +0600

    telebot com12 headers

--- Part2_Module20/main.py
+++ Part2_Module20/main.py
@@ -2,13 +2,40 @@
 # «Привет» (необходимо сообщить проверяющему куратору имя вашего бота для
 # тестирования).
 
-import json
 import telebot
 import requests
 from config import USERNAME, BOT_TOKEN, API_KEY
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
+
+
+bot.message_handler(commands=["random"])
+def random_film(message):
+    api_url = "https://api.kinopoisk.dev/v1.4/movie/random"
+    headers = {"accept": "application/json",
+              "X-API-KEY": API_KEY
+              }
+
+    response = requests.get(api_url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        # Извлечь нужные данные из объекта `data`:
+        title = data.get("name")
+        # rating = data.get("rating")
+
+
+
+        bot.send_message(message.chat.id, response.text)
+    else:
+         print("Ошибка соединения")
+          
+
+
+
+
+
 
 @bot.message_handler(commands=["get_data"])
 def get_data(message):
@@ -20,8 +47,6 @@ def get_data(message):
     if response.status_code == 200:
             data = response.json()
             bot.send_message(message.chat.id, f"Получены данные: {data}")
-
-
 
 @bot.message_handler(commands=["hello-world"])
 def greet_message(message):
