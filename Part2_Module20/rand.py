12d502c3ab0920cf6e405148b893370b4c922ce6
Octopus <klinov.artem@gmail.com>
2023-12-31 18:05:40 +0600

    telebot com23

--- Part2_Module20/rand.py
+++ Part2_Module20/rand.py
@@ -9,40 +9,31 @@ bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(commands=["random"])
 def get_random_film(message):
+
     url = "https://api.kinopoisk.dev/v1.4/movie/random"
+    # url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=3&type=movie&rating.kp=6-10"
     headers = {"accept": "application/json", "X-API-KEY": API_KEY}
     
-    while True:
-        try:
-            response = requests.get(url, headers=headers)
-            response.raise_for_status()
+    response = requests.get(url, headers=headers)
 
-            if response.status_code == 200:
-                data = response.json()
+    if response.status_code == 200:
+        data = response.json()
                 
-                # Извлечь нужные данные из объекта `data`:
-                title = data.get("name")
-                rating = data.get("rating", {})
-                kp_rate = rating.get("kp", "N/A")
-                year = data.get("year")
-                print(f"Фильм: {title}\n" 
-                    f"Рейтинг КП: {kp_rate}\n"
-                    f"год: {year}\n")
                 
-                if title and kp_rate > 7:
-                    bot.send_message(message.chat.id,   f"Фильм с рейтингом больше 7:\n"
+        # Извлечь нужные данные из объекта `data`:
+        title = data.get("name")
+        rating = data.get("rating", {})
+        kp_rate = rating.get("kp", "N/A")
+        year = data.get("year")
+        print(f"Фильм: {title}\n" 
+                f"Рейтинг КП: {kp_rate}\n"
+                f"год: {year}\n")
+                
+        bot.send_message(message.chat.id,   f"Фильм с рейтингом больше 7:\n"
                                                         f"{title}\n" 
                                                         f"Рейтинг КП: {kp_rate}\n"
                                                         f"год: {year}\n")
-                    break
-            else:
-                bot.send_message(message.chat_id, f'''Ошибка при получении данных. 
-                                Код ответа: {response.status_code}''')
-                break
 
-        except requests.exceptions.RequestException as exc:
-            bot.send_message(message.chat.id, f'''Произошла ошибка при получении 
-                            случайного фильма: {exc}''')
 
 if __name__ == "__main__":
     bot.infinity_polling()
