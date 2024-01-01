3461559d3480c02fb1978acc8152b6eb1afebf93
Octopus <klinov.artem@gmail.com>
2024-01-01 15:32:14 +0600

    telebot com25 keyboard

--- Part2_Module20/low.py
+++ Part2_Module20/low.py
@@ -1,25 +1,22 @@
 
 import telebot
 import requests
+from telebot import types
 from config import USERNAME, BOT_TOKEN, API_KEY
 
 
 bot = telebot.TeleBot(BOT_TOKEN)
 cached_movie = set()
 
-
+@bot.message_handler(commands=["new"])
 def ask_user(message):
+
+    chat_id = message.chat.id
+    keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    keyboard.add(types.KeyboardButton("Фильмы"))
+    keyboard.add(types.KeyboardButton("Сериалы"))
+    keyboard.add(types.KeyboardButton("Мультфильмы"))
+
     
 
-@bot.message_handler(commands=["low"])
-def get_low_movies(message):
 
-    url = "https://api.kinopoisk.dev/v1.4/movie"
-    headers = {"accept": "application/json", "X-API-KEY": API_KEY}
-
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-
-
-        ask_user(message)
