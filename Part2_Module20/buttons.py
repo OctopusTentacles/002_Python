acc4d17be4a62fc1e6904f478f3ba8b00398f884
Octopus <klinov.artem@gmail.com>
2024-01-01 22:20:41 +0600

    telebot com30 buttons

--- Part2_Module20/buttons.py
+++ Part2_Module20/buttons.py
@@ -3,7 +3,7 @@ import telebot
 import requests
 from telebot import types
 from config import USERNAME, BOT_TOKEN, API_KEY
-
+from new import get_new_movies
 
 bot = telebot.TeleBot(BOT_TOKEN)
 cached_movie = set()
@@ -20,24 +20,26 @@ def ask_user(message):
     keyboard.add(types.KeyboardButton("Мультфильмы"))
 
     # запрос у пользователя:
-    bot.send_message(chat_id, "Выбири тип", reply_markup=keyboard)
-    bot.register_next_step_handler(message, user_choise)
+    msg = bot.send_message(chat_id, "Выбири тип", reply_markup=keyboard)
+    bot.register_next_step_handler(msg, user_choise, get_new_movies)
 
 
-def user_choise(message):
+def user_choise(message, next_function):
     chat_id = message.chat.id
     user_choice = message.text.lower()
 
+    url = None
     if user_choice == "фильмы":
         url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=movie&year=2023"
 
-    if user_choice == "сериалы":
+    elif user_choice == "сериалы":
         url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=tv-series&year=2023"
 
-    if user_choice == "мультфильмы":
+    elif user_choice == "мультфильмы":
         url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=cartoon&year=2023"
 
-    return url
+    if url is not None:
+        next_function(chat_id, url)
 
 
 
