29165c95e26f1c3bc61f199e585fd944f79ce03d
Octopus <klinov.artem@gmail.com>
2024-01-03 02:26:51 +0600

    telebot com37

--- Part2_Module20/buttons.py
+++ Part2_Module20/buttons.py
@@ -17,14 +17,11 @@ def ask_user(message):
     # кнопки выбора:
     print("выбираю кнопку")
     keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    keyboard.add(types.KeyboardButton("Фильмы"))
-    keyboard.add(types.KeyboardButton("Сериалы"))
-    keyboard.add(types.KeyboardButton("Мультфильмы"))
+    keyboard.add("Фильмы", "Сериалы", "Мультфильмы")
 
     # запрос у пользователя нажать кнопку:
-    bot.send_message(user_id, "Выбири тип", reply_markup=keyboard)
-    # bot.register_next_step_handler(message, user_choice)
-    print("запуск функции с парам", message.text)
+    bot.send_message(message.chat.id, "Выбири тип", reply_markup=keyboard)
+    bot.register_next_step_handler(message, user_choice)
 
 
     # скрываем клавиатуру после выбора пользователя
@@ -33,45 +30,28 @@ def ask_user(message):
 
 
 
-
-# @bot.message_handler(func=lambda message: True)
-# def all_message(message):
-#     if message.text.lower() in ["фильмы", "сериалы", "мультфильмы"]:
-#         user_choice(message)
-#     else:
-#         bot.send_message(message.chat.id, "Неизвестная команда. Пожалуйста, используйте кнопки.")
-
-# @bot.message_handler(content_types=["KeyboardButton"])
-# def user_movie(message):
-#         print(message.text)
-#         url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=movie&year=2023"
-#         print("https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=movie&year=2023")
-#         return url
-
-
-
-@bot.message_handler(func=lambda message: message.text.lower() in ["фильмы", "сериалы", "мультфильмы"])
 def user_choice(message):
-    print("запуск функции с парам", message.text)
-    user_id = message.from_user.id
-    user_type = message.text.lower()    
-    print("функцию get_new_movies в параметре ")
+    print(message)
+    print(message.text)
+    # print("запуск функции с парам")
+    # user_type = message.text.lower()    
+    # print("функцию get_new_movies в параметре ")
 
 
-    url = None
-    if user_type == "Фильмы":
-        url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=movie&year=2023"
-        print("https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=movie&year=2023"
-)
-    elif user_type == "Сериалы":
-        url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=tv-series&year=2023"
+    # url = None
+    # if call.data == "button_m":
+    #     url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=movie&year=2023"
+    #     print("https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=movie&year=2023")
 
-    elif user_type == "Мультфильмы":
-        url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=cartoon&year=2023"
+    # elif user_type == "Сериалы":
+    #     url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=tv-series&year=2023"
 
-    if url is not None:
-        get_new_movies(chat_id, url)
+    # elif user_type == "Мультфильмы":
+    #     url = "https://api.kinopoisk.dev/v1.4/movie?page=1&limit=5&type=cartoon&year=2023"
+
+    # if url is not None:
+    #     get_new_movies(chat_id, url)
 
 
-if __name__ == "__main__":
-    bot.infinity_polling()
+# if __name__ == "__main__":
+#     bot.infinity_polling()
