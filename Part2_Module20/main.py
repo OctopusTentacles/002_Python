a501dce84bf8151d7e437bc6bbc2d7fa69413bc6
Octopus <klinov.artem@gmail.com>
2024-01-05 14:39:17 +0600

    telebot com48 flake8 for main

--- Part2_Module20/main.py
+++ Part2_Module20/main.py
@@ -1,3 +1,6 @@
+"""Этот модуль содержит основной код для бота Telegram."""
+
+
 import telebot
 
 from buttons import get_main_keyboard
@@ -8,33 +11,48 @@ from new import get_new_url
 bot = telebot.TeleBot(BOT_TOKEN)
 
 
-@bot.message_handler(commands=["start"])
-def welcome(message):
+@bot.message_handler(commands=['start'])
+def welcome(message: telebot.types.Message) -> None:
+    """Приветственное сообщение бота. Реагирует при запуске и по команде /start.
+
+    Args:
+        message (telebot.types.Message): Сообщение от пользователя.
+    """
     username = message.from_user.first_name
     keyboard = get_main_keyboard()
-    bot.send_message(message.chat.id, f"Привет, {username}!\n"
-                     f"Выбери одну из команд:", reply_markup=keyboard)
-
+    bot.send_message(message.chat.id,
+                     f'Привет, {username}!\nВыбери одну из команд:',
+                     reply_markup=keyboard
+                     )
 
 @bot.callback_query_handler(func=lambda call: True)
-def main_menu(call):
+def main_menu(call: telebot.types.CallbackQuery) -> None:
+    """Основное меню бота.
 
+    Args:
+        call (telebot.types.CallbackQuery): Callback-запрос от пользователя.
+    """
     category = None
 
-    if call.data == "новинки":
+    if call.data == 'новинки':
         ask_user_buttons(call)
 
-    elif call.data in ["фильм", "сериал", "мульт", "main"]:
+    elif call.data in ['фильм', 'сериал', 'мульт', 'main']:  # noqa: WPS510
         category = call.data
 
     if category is not None:
         get_new_url(call.message.chat.id, category)
 
 
-def ask_user_buttons(call):
+def ask_user_buttons(call: telebot.types.CallbackQuery) -> None:
+    """Запрос от бота для выбора типа контента.
+
+    Args:
+        call (telebot.types.CallbackQuery): Callback-запрос от пользователя.
+    """
     keyboard = get_new_keyboard()
-    bot.send_message(call.message.chat.id, "Выбери тип", reply_markup=keyboard)
+    bot.send_message(call.message.chat.id, 'Выбери тип', reply_markup=keyboard)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     bot.infinity_polling()
