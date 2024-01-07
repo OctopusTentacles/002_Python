c223fbb8bb0737d5dc8df37511cdd1d3b7a33ae3
Octopus <klinov.artem@gmail.com>
2024-01-07 20:30:08 +0600

    telebot com55 history.py

---
+++ Part2_Module20/history.py
@@ -0,0 +1,59 @@
+from typing import List
+from models import UserRequest
+from telebot.types import CallbackQuery
+from telebot import TeleBot
+
+
+def show_history(bot: TeleBot, call: CallbackQuery) -> None:
+    """Выводит краткую историю запросов пользователя.
+
+    Args:
+        bot (TeleBot): Экземпляр бота.
+        call (CallbackQuery): Callback-запрос от пользователя.
+    """
+
+    # получаем уникальный идентификатор пользователя в str
+    # для последующего использования, например, при сохранении в
+    # базу данных или других операциях, где ожидается строковый формат.
+    user_id = str(call.message.from_user.id)
+    histoty_entries = get_user_history(user_id)
+
+    if histoty_entries:
+        history_text = '\n'.join(histoty_entries)
+        bot.send_message(call.message.chat.id,
+                         f'История запросов пользователя:\n{history_text}'
+                         )
+    else:
+        bot.send_message(call.message.chat.id,
+                         f'История запросов пользователя пуста.'
+                         )
+
+def get_user_history(user_id: str) -> List[str]:
+    """Получаем историю запросов пользователя.
+
+    Args:
+        user_id (str): Идентификатор пользователя.
+
+    Returns:
+        List[str]: Список строк с историей запросов пользователя.
+    """
+    try:
+        history_entries = (
+            UserRequest
+            .select()
+            .where(UserRequest.user_id == user_id)
+            .order_by(UserRequest.timestamp.desc())
+            .limit(10)
+        )
+        history_text = [
+            f'{entry.timestamp.strftime("%Y-%m-%d %H:%M:%S")} - {entry.category}'
+            for entry in history_entries
+        ]
+
+
+        return history_text
+    except Exception as exc:
+        print(f'Ошибка получения истории пользователя: {exc}')
+        return []
+
+
