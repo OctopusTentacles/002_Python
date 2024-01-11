969e8a599b933084f5c38d37371f8be5e33b14df
Octopus <klinov.artem@gmail.com>
2024-01-12 02:26:49 +0600

    telebot com74 user_state

--- Part2_Module20/user_state.py
+++ Part2_Module20/user_state.py
@@ -18,4 +18,22 @@ def user_state(wrapped, instance, args, kwargs):
     """
 
     # получаем ID пользователя из сообщения:
-    user_id = 
+    user_id = instance.from_user.id
+
+    # Получение атрибута user_states из экземпляра класса. 
+    # Если атрибут не существует, используется пустой словарь.
+    instance.user_states = getattr(instance, 'user_states', {})
+
+    # Получение состояния пользователя из словаря user_states. 
+    # Если состояние пользователя не существует, 
+    # используется пустой словарь.
+    instance.user_states[user_id] = getattr(
+        instance, 'user_states', {}
+    ).get(user_id, {})
+
+    # Установка категории состояния пользователя. 
+    # используем значение "start", если категория не была 
+    # передана в виде аргумента.
+    instance.user_states[user_id]['category'] = kwargs.get('caategory', 'start')
+
+    return wrapped(*args, **kwargs)
