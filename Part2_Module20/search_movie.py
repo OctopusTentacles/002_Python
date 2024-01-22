70cf71048ebefd202519dbb3c7e10da0b8c150a0
Octopus <klinov.artem@gmail.com>
2024-01-22 16:00:21 +0500

    telebot com116 search encoding_title

--- Part2_Module20/search_movie.py
+++ Part2_Module20/search_movie.py
@@ -12,6 +12,8 @@ from buttons import get_new_keyboard
 from logger import logger
 from models import UserRequest
 
+from urllib.parse import quote
+
 # from config import API_KEY
 # from config import BOT_TOKEN
 
@@ -19,13 +21,22 @@ from models import UserRequest
 user_response = {}
 
 
+
 def user_input_title(bot: TeleBot, call: CallbackQuery,
                  user_name: str, user_id: str):
-    """"""  
+    """Отправляет сообщение пользователю и 
+    регистрирует следующий шаг обработчика ввода.
+
+    Args:
+        bot (TeleBot): Экземпляр бота.
+        call (CallbackQuery): Callback-запрос от пользователя.
+        user_name (str): Имя пользователя.
+    """  
 
     bot.send_message(call.message.chat.id, 'Введи название фильма')
     bot.register_next_step_handler(call.message, create_url)
         
+
 
 def create_url(call):
 
@@ -39,13 +50,18 @@ def create_url(call):
     )
     print(user_response[chat_id])
 
+    # Получить введенное пользователем название и закодировать:
+    user_title = call.text.strip()
+    encoding_title = quote(user_title)
+
     url = (
         f'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=1&'
-        f'query={call.text.strip()}'
+        f'query={encoding_title}'
     )
 
-    print('ссылка поиска', url)
+
+def get_search_content():
 
 
-# if __name__ == '__main__':
-#     bot.infinity_polling()
+if __name__ == '__main__':
+    bot.infinity_polling()
