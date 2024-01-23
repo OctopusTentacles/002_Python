3b521e132e6033d12a1a81ad43f943b5c38616dc
Octopus <klinov.artem@gmail.com>
2024-01-23 12:00:18 +0500

    telebot com118

--- Part2_Module20/search_movie.py
+++ Part2_Module20/search_movie.py
@@ -12,20 +12,34 @@ from buttons import get_new_keyboard
 from logger import logger
 from models import UserRequest
 
-# from config import API_KEY
-# from config import BOT_TOKEN
+from urllib.parse import quote
 
+from config import API_KEY
+from config import BOT_TOKEN
+
+
+bot = telebot.TeleBot(BOT_TOKEN)
 
 user_response = {}
 
 
-def user_input_title(bot: TeleBot, call: CallbackQuery,
+
+def user_input_title(call: CallbackQuery,
                  user_name: str, user_id: str):
-    """"""  
+    """Отправляет сообщение пользователю и 
+    регистрирует следующий шаг обработчика ввода.
+
+    Args:
+        bot (TeleBot): Экземпляр бота.
+        call (CallbackQuery): Callback-запрос от пользователя.
+        user_name (str): Имя пользователя.
+    """  
 
     bot.send_message(call.message.chat.id, 'Введи название фильма')
     bot.register_next_step_handler(call.message, create_url)
         
+
+
 
 def create_url(call):
 
@@ -39,13 +53,38 @@ def create_url(call):
     )
     print(user_response[chat_id])
 
+    # Получить введенное пользователем название и закодировать:
+    user_title = call.text.strip()
+    encoding_title = quote(user_title)
+
     url = (
         f'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=1&'
-        f'query={call.text.strip()}'
+        f'query={encoding_title}'
     )
 
-    print('ссылка поиска', url)
+
+    get_search_content(chat_id, url)
+    keyboard = get_new_keyboard()
+    bot.send_message(
+        call.message.chat.id, 'МЕНЮ НОВИНКИ   '
+        'Выбери тип новинок или вернись в главное меню:', 
+        reply_markup=keyboard
+    )
 
 
-# if __name__ == '__main__':
-#     bot.infinity_polling()
+
+
+def get_search_content(chat_id, url):
+    headers = {'accept': 'application/json', 'X-API-KEY': API_KEY}
+    response = requests.get(url, headers=headers)
+
+    if response.status_code == 200:
+        data = response.json()
+        contents = data.get('docs')
+
+        print('docs')
+
+    pass
+
+if __name__ == '__main__':
+    bot.infinity_polling()
