b15acdbb2e1bc11a819cb5725e8fe40db6d3a211
Octopus <klinov.artem@gmail.com>
2024-01-23 12:00:19 +0500

    telebot com118

--- Part2_Module20/search_movie.py
+++ Part2_Module20/search_movie.py
@@ -5,6 +5,7 @@ from io import BytesIO
 
 from telebot import TeleBot
 from telebot.types import CallbackQuery
+from urllib.parse import quote
 
 
 from buttons import get_main_keyboard
@@ -12,36 +13,27 @@ from buttons import get_new_keyboard
 from logger import logger
 from models import UserRequest
 
-from urllib.parse import quote
-
 from config import API_KEY
 from config import BOT_TOKEN
 
 
-bot = telebot.TeleBot(BOT_TOKEN)
-
+# bot = telebot.TeleBot(BOT_TOKEN)
 user_response = {}
 
 
-
-def user_input_title(call: CallbackQuery,
-                 user_name: str, user_id: str):
-    """Отправляет сообщение пользователю и 
-    регистрирует следующий шаг обработчика ввода.
-
-    Args:
-        bot (TeleBot): Экземпляр бота.
-        call (CallbackQuery): Callback-запрос от пользователя.
-        user_name (str): Имя пользователя.
+def user_input_title(bot: telebot, call: CallbackQuery):
+    """отправляет сообщение пользователю и регистрирует следующий шаг 
+    обработчика ввода
     """  
 
     bot.send_message(call.message.chat.id, 'Введи название фильма')
-    bot.register_next_step_handler(call.message, create_url)
+    bot.register_next_step_handler(call.message, create_url, bot)
         
 
 
 
-def create_url(call):
+
+def create_url(call, bot):
 
     chat_id = call.chat.id
 
@@ -51,9 +43,7 @@ def create_url(call):
     logger.info(
         f'Пользователь ищет фильм по названию {call.text.strip()}.'
     )
-    print(user_response[chat_id])
 
-    # Получить введенное пользователем название и закодировать:
     user_title = call.text.strip()
     encoding_title = quote(user_title)
 
@@ -62,29 +52,17 @@ def create_url(call):
         f'query={encoding_title}'
     )
 
+    print('ссылка поиска', url)
 
-    get_search_content(chat_id, url)
-    keyboard = get_new_keyboard()
-    bot.send_message(
-        call.message.chat.id, 'МЕНЮ НОВИНКИ   '
-        'Выбери тип новинок или вернись в главное меню:', 
-        reply_markup=keyboard
-    )
+    get_search_movie(bot, call, url)
 
 
+def get_search_movie(bot, call, url):
+    print('in def get_search_movie')
+
+    bot.send_message(call.message.chat.id, 'сейчас найдем')
 
 
-def get_search_content(chat_id, url):
-    headers = {'accept': 'application/json', 'X-API-KEY': API_KEY}
-    response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
-        contents = data.get('docs')
-
-        print('docs')
-
-    pass
-
-if __name__ == '__main__':
-    bot.infinity_polling()
+# if __name__ == '__main__':
+#     bot.infinity_polling()
