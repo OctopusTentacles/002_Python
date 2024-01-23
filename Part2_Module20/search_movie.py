df5006bb123277adcdee4fd94423641a740fbb6c
Octopus <klinov.artem@gmail.com>
2024-01-23 17:02:04 +0500

    telebot com122

--- Part2_Module20/search_movie.py
+++ Part2_Module20/search_movie.py
@@ -15,29 +15,27 @@ from models import UserRequest
 from config import API_KEY
 from config import BOT_TOKEN
 
-from config import API_KEY
-from config import BOT_TOKEN
-
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
-# bot = telebot.TeleBot(BOT_TOKEN)
 user_response = {}
 
 
 def user_input_title(bot: telebot, call: CallbackQuery):
     """отправляет сообщение пользователю и регистрирует следующий шаг 
-    обработчика ввода
+    обработчика ввода.
+    
+    Args:
+        bot (TeleBot): Экземпляр бота.
+        call (CallbackQuery): Callback-запрос от пользователя.
     """  
 
     bot.send_message(call.message.chat.id, 'Введи название фильма')
     bot.register_next_step_handler(call.message, create_url, bot)
-        
-
-
 
 
 def create_url(call, bot):
+    """Обработчик ввода, формирует URL для поиска фильма."""
 
     chat_id = call.chat.id
 
@@ -59,16 +57,13 @@ def create_url(call, bot):
         f'https://api.kinopoisk.dev/v1.4/movie/search?page=1&limit=1&'
         f'query={encoding_title}'
     )
-
-
     get_search_content(bot, url, chat_id)
     keyboard = get_main_keyboard()
     bot.send_message(chat_id, "ГЛАВНОЕ МЕНЮ", reply_markup=keyboard)
 
 
-
 def get_search_content(bot, url, chat_id):
-
+    """Выполняет поиск фильма и отправляет результат пользователю."""
     bot.send_message(chat_id, 'сейчас найдем')
 
     headers = {"accept": "application/json", "X-API-KEY": API_KEY}
@@ -96,7 +91,6 @@ def get_search_content(bot, url, chat_id):
             rate_imdb = content.get('rating', {}).get('imdb')
 
 
-
             image_io = BytesIO(requests.get(poster).content)
             message_text = (
                 f'{title}   ({year})\n\n'
@@ -106,10 +100,19 @@ def get_search_content(bot, url, chat_id):
                 f'длительность: {length} мин.\n\n'
                 f'КП: {rate_kp}\n'
                 f'IMDB: {rate_imdb}'
-
             )
 
             bot.send_photo(chat_id, image_io, caption=message_text)
+            logger.info(
+                f'Пользователь получил данные по: {title}.'
+            )
+
+
+    else:
+        logger.error(
+            f'Ошибка при получении данных. Код ответа: {response.status_code}'
+        )
+        bot.send_message(chat_id, 'Прости, неполадки, давай еще раз...')
 
 
 if __name__ == '__main__':
