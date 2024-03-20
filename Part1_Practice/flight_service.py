c6523b9cb5ae12a951e557c8a7281bc9a8dda93c
Octopus <klinov.artem@gmail.com>
2024-03-20 21:14:26 +0500

    practice_com3 main_menu

--- Part1_Practice/flight_service.py
+++ Part1_Practice/flight_service.py
@@ -1,18 +1,44 @@
 
 
 
+def create_flight():
+    ...
+
+
+
+def show_flights():
+    ...
+
+
+
+def search_flight():
+    ...
+
 
 
 # MAIN MENU
-while True:    
-    print('Сервис поиска авиабилетов\n\n')
-    print('Главное меню:\n')
-    print('1 - ввод рейса')
-    print('2 - вывод всех рейсов')
-    print('3 - поиск рейса по номеру')
-    print('0 - завершение работы')
+def main_menu():
+    while True:    
+        print('Сервис поиска авиабилетов\n\n')
+        print('Главное меню:\n')
+        print('1 - ввод рейса')
+        print('2 - вывод всех рейсов')
+        print('3 - поиск рейса по номеру')
+        print('0 - завершение работы')
+
+        option = int(input('\nВведите номер пункта меню: '))
+        if option == 1:
+            create_flight()
+        elif option == 2:
+            show_flights()
+        elif option == 3:
+            search_flight()
+        elif option == 0:
+            print('\nПрограмма завершена!\n')
+            break
+        else:
+            print('\nОшибка! Повторите ввод!\n')
 
 
-    option = int(input('Введите номер пункта меню: '))
-    if option == 0:
-        break
+
+main_menu()
