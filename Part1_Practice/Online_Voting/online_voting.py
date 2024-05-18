8e0251ecdf9cd9cc470a89442f86983673e3e021
Octopus <klinov.artem@gmail.com>
2024-05-18 16:36:47 +0500

    online_voting com15

--- Part1_Practice/Online_Voting/online_voting.py
+++ Part1_Practice/Online_Voting/online_voting.py
@@ -4,7 +4,7 @@
 # Создать новое голосование.
 # Функция для создания нового голосования____________________________
 def create_voting():
-    print('Голосование за автомобиль года.\n')
+    print('\nГолосование за автомобиль года.\n')
 
     count_models = int(input(
         'Сколько моделей авто участвуют в голосовании?: '
@@ -13,27 +13,27 @@ def create_voting():
     # словарь для хранения моделей авто:
     models = list()
     for i in range(1, count_models + 1):
-        new_model = input(f'\nВведите модель {i}-го автомобиля: ')
+        new_model = input(f'Введите модель {i}-го автомобиля: ')
 
         models.append(new_model)
 
-        print('\nГолосование создано!')
+    print('\nГолосование создано!')
 
-        return models
+    return models
     
 # Проведение голосования.
 # Функция для проведения голосования_________________________________
 def conduct_voting(models):
-    print('Выберите модель из списка:', ';'.join(models))
-    print('Для подсчета голосов введите 0')
+    print('Выберите модель из списка:', '; '.join(models))
+    print('Для подсчета голосов введите 0\n')
 
     # список для подсчета голосов на каждую модель,
     # список из 0 = длине models, [0, 0, 0]:
     votes = [0] * len(models)
 
     while True:
-        choice = input('Ваш выбор?:')
-        if choice == 0:
+        choice = input('\nВаш выбор?: ')
+        if choice == '0':
             break
         elif choice in models:
             index = models.index(choice)
@@ -48,12 +48,12 @@ def conduct_voting(models):
 def calculate_results(models, votes):
     '''Функция остановки голосования и получения результата. '''
 
-    print("Голосование завершено!")
+    print('\nГолосование завершено!')
 
     max_votes = max(votes)
     winner_index = votes.index(max_votes)
-    print("Лучший автомобиль года:", models[winner_index])
-    print("Количество голосов:", max_votes)
+    print('Лучший автомобиль года:', models[winner_index])
+    print('Количество голосов:', max_votes)
 
 
 # MAIN===============================================================
