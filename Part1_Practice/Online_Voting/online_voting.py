b32d197e62109ed9c865ca41b1837c84ced32d5e
Octopus <klinov.artem@gmail.com>
2024-05-18 16:16:34 +0500

    online_voting com11 conduct_voting

--- Part1_Practice/Online_Voting/online_voting.py
+++ Part1_Practice/Online_Voting/online_voting.py
@@ -31,10 +31,20 @@ def conduct_voting(models):
     print('Выберите модель из списка:', ';'.join(models))
     print('Для подсчета голосов введите 0')
 
-    # список для подсчета голосов на каждую модель:
+    # список для подсчета голосов на каждую модель,
+    # список из 0 = длине models, [0, 0, 0]:
     votes = [0] * len(models)
 
     while True:
         choice = input('Ваш выбор?:')
         if choice == 0:
             break
+        elif choice in models:
+            index = models.index(choice)
+            votes[index] += 1
+            print('Ваш голос принят!')
+        else:
+            print('Выберите модель из списка!')
+
+    return votes
+
