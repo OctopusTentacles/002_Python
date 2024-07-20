a2cc0cb8c06b419c4dc3b4f0314d194cd400464c
Octopus <klinov.artem@gmail.com>
2024-07-20 21:56:50 +0500

    training_sort_1.com2

--- TRAINING/Sorting/task_1.py
+++ TRAINING/Sorting/task_1.py
@@ -9,7 +9,16 @@ from typing import List
 logger = logging.getLogger('Bubble_Sort')
 
 def bubble_sort(arr: List[int]) -> List[int]:
-    ...
+    len_arr = len(arr)
+
+    for i in range(len_arr):
+        logger.info(f'\tИтерация {i+1}: массив {arr}')
+        
+        for j in range(0, len_arr-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
 
 
 
