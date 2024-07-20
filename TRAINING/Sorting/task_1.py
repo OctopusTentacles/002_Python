7e0e6d62607fb0ee1e5925dbef53bb0cd8090352
Octopus <klinov.artem@gmail.com>
2024-07-20 22:05:34 +0500

    training_sort_1.com3

--- TRAINING/Sorting/task_1.py
+++ TRAINING/Sorting/task_1.py
@@ -13,10 +13,12 @@ def bubble_sort(arr: List[int]) -> List[int]:
 
     for i in range(len_arr):
         logger.info(f'\tИтерация {i+1}: массив {arr}')
-        
+
         for j in range(0, len_arr-i-1):
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+        
+        logger.info(f'\tперестановка с {j} на {j+1}')
 
     return arr
 
@@ -26,8 +28,8 @@ if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
     logger.info('Запуск пузырьковой сортировки')
 
-    arr = [64, 34, 25, 12, 22, 11, 90]
+    arr = [64, 34, 25, 12, 22, 11, 90, 5]
     logger.info(f'Текущий массив: {arr}')
 
     sorted_arr = bubble_sort(arr)
-    logger.info(f'Отсортированный массив: {sorted_arr}')
+    logger.info(f'\tОтсортированный массив: {sorted_arr}')
