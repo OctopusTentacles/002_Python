22f652826daa67353b6f00e77b497ad7cc3950f9
Octopus <klinov.artem@gmail.com>
2024-07-21 21:12:03 +0500

    training_sort_1.com9

--- TRAINING/Sorting/task_2.py
+++ TRAINING/Sorting/task_2.py
@@ -15,24 +15,20 @@ logger = logging.getLogger('revers_bubble_sort')
 def revers_bubble_sort(arr: List[int]) -> List[int]:
     len_arr = len(arr)
 
-    for i in range(len_arr):
+    for i in range(1, len_arr-1):
         swapped = False
         logger.info(f'\tИтерация {i}')
 
-        for j in range(i, len_arr-1):
+        for j in range(0, len_arr-1):
             if arr[j] < arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
                 logger.info(f'\tСортировка {arr}')
         if not swapped:
-            logger.info(f'\tСортировка завершилась на {i} итерации.')
+            logger.info(f'\tСортировка завершилась на {i-1} итерации.')
             break
     
     return arr
-
-
-
-
 
 
 if __name__ == '__main__':
