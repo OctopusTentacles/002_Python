e22e214f33dae19c585d59b3249f3cac4989db88
Octopus <klinov.artem@gmail.com>
2024-07-21 20:05:56 +0500

    training_sort_1.com4

--- TRAINING/Sorting/task_1.py
+++ TRAINING/Sorting/task_1.py
@@ -10,26 +10,26 @@ logger = logging.getLogger('Bubble_Sort')
 
 def bubble_sort(arr: List[int]) -> List[int]:
     len_arr = len(arr)
-
+    # Внешний цикл по всем элементам массива
     for i in range(len_arr):
         logger.info(f'\tИтерация {i+1}: массив {arr}')
-
+        # Внутренний цикл, который проходит по неотсортированной части массива
         for j in range(0, len_arr-i-1):
+            # Если текущий элемент больше следующего, меняем их местами
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
         
-        logger.info(f'\tперестановка с {j} на {j+1}')
+        logger.info(f'\tперестановка: {arr}')
 
     return arr
 
 
-
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
-    logger.info('Запуск пузырьковой сортировки')
+    logger.info('\tЗапуск пузырьковой сортировки')
 
     arr = [64, 34, 25, 12, 22, 11, 90, 5]
-    logger.info(f'Текущий массив: {arr}')
+    logger.info(f'\tТекущий массив: {arr}')
 
     sorted_arr = bubble_sort(arr)
     logger.info(f'\tОтсортированный массив: {sorted_arr}')
