fa356ace3f6007b4b33e7c8ec3fbac4573e7728e
Octopus <klinov.artem@gmail.com>
2024-07-21 20:25:23 +0500

    training_sort_1.com5

--- TRAINING/Sorting/task_1.py
+++ TRAINING/Sorting/task_1.py
@@ -10,15 +10,26 @@ logger = logging.getLogger('Bubble_Sort')
 
 def bubble_sort(arr: List[int]) -> List[int]:
     len_arr = len(arr)
+    
     # Внешний цикл по всем элементам массива
     for i in range(len_arr):
         logger.info(f'\tИтерация {i+1}: массив {arr}')
+
+        # флаг, который отслеживает, были ли сделаны перестановки 
+        # на текущей итерации. 
+        swapped = False
+
         # Внутренний цикл, который проходит по неотсортированной части массива
         for j in range(0, len_arr-i-1):
             # Если текущий элемент больше следующего, меняем их местами
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-        
+                swapped = True
+        # Если нет перестановок, массив уже отсортирован, 
+        # и можно завершить выполнение раньше.
+        if not swapped:
+            break
+
         logger.info(f'\tперестановка: {arr}')
 
     return arr
