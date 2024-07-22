e7fad0232326e7ca3091d164e16f765033f8b746
Octopus <klinov.artem@gmail.com>
2024-07-22 17:28:10 +0500

    training_sort_3.com4

--- TRAINING/Sorting/task_3.py
+++ TRAINING/Sorting/task_3.py
@@ -12,6 +12,7 @@ from typing import List
 logger = logging.getLogger('sorting_time')
 
 def bubble_sort(arr: List[int]) -> List[int]:
+    logger.info('\tПузырьковая сортировка:')
     len_arr = len(arr)
 
     for i in range(len_arr):
@@ -28,11 +29,22 @@ def bubble_sort(arr: List[int]) -> List[int]:
 
     return arr
 
+
 def quick_sort(arr: List[int]) -> List[int]:
-    ...
+    if len(arr) <= 1:
+        return arr
+    
+    # Выбор опорного элемента (pivot):
+    pivot = arr[len(arr) // 2]
 
+    # Разделение массива:
+    left = [i for i in arr if i < pivot]
+    middle = [i for i in arr if i == pivot]
+    right = [i for i in arr if i > pivot]
 
-
+    # Рекурсивная сортировка и объединение:
+    return quick_sort(left) + middle + quick_sort(right)
+    
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
@@ -40,6 +52,7 @@ if __name__ == '__main__':
     # генерация случайного массива:
     array_size = 1000
     array = [random.randint(0, 1000) for _ in range(array_size)]
+    logger.info(f'{array}')
 
     # копирование массива для каждой сортировки:
     copy_arr_for_bubble = array.copy()
@@ -49,12 +62,12 @@ if __name__ == '__main__':
     start_time = time.time()
     arr_bubble_sort = bubble_sort(copy_arr_for_bubble)
     time_bubble_sort = time.time() - start_time
-    logger.info(f'\tПузырьковая сортировка: {arr_bubble_sort}')
+    logger.info(f'\tмассив отсортирован: {arr_bubble_sort}')
     logger.info(f'\tЗатраченное время {round(time_bubble_sort, 3)} сек.\n')
 
     # измерение времени быстрой сортировки:
     start_time = time.time()
     arr_quick_sort = quick_sort(copy_arr_for_quick)
     time_quick_sort = time.time() - start_time
-    logger.info(f'\Быстрая сортировка: {arr_quick_sort}')
+    logger.info(f'\массив отсортирован: {arr_quick_sort}')
     logger.info(f'\tЗатраченное время {round(time_quick_sort, 3)} сек.\n')
